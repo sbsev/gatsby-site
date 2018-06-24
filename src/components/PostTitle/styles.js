45292003bd9170120c671ded69ba1010b873174f
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-06-24 11:04:38 +0200

    style: post title

--- src/components/PostTitle/styles.js
+++ src/components/PostTitle/styles.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
 
-export const Layout = styled.div`
-  text-align: center;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
 
 export const Title = styled.h1`
@@ -12,6 +14,10 @@ export const Title = styled.h1`
 
 export const Meta = styled.div`
   font-size: 0.9rem;
+  width: fit-content;
+  padding: 0 0.6rem;
   margin: 0.75rem 0 1.5rem;
   color: ${props => props.theme.mainGreen};
+  border: ${({theme}) => theme.mediumBorder + ' solid ' + theme.darkBlue};
+  border-radius: ${props => props.theme.largeBorderRadius};
 `
