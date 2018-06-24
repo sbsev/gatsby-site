3d3d4fa3be1c4e95eb0cb71243b122ba49280322
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-06-24 18:31:10 +0200

    feat: yarn add gatsby-plugin-catch-links for local markdown links without page refresh

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,7 @@ module.exports = {
         ],
       },
     },
+    `gatsby-plugin-catch-links`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
