b598f967e02d068e45ef3550486ab1e5644f0395
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-06-26 23:08:21 +0200

    feat: add BlogIndexLayout

---
+++ src/components/BlogIndexLayout.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+import mediaQuery from '../utils/mediaQuery'
+
+const BlogIndexLayout = styled.div`
+  ${mediaQuery.minPhone} {
+    display: grid;
+    grid-template-columns: 1fr min-content;
+    grid-gap: 1rem;
+    ${mediaQuery.minTablet} {
+      grid-gap: 3rem;
+    }
+  }
+`
+
+export default BlogIndexLayout
