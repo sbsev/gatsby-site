b35aacc626156e6073e1427ef402dfa4050fb3e5
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-06-26 23:09:08 +0200

    feat: CategoryList comp

---
+++ src/components/CategoryList/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { List, ListTitle, CategoryLink } from './styles'
+
+const Category = ({ category, active }) => (
+  <li>
+    <CategoryLink active={active} to={`/blog/category/` + category.slug}>
+      {category.title}
+    </CategoryLink>
+  </li>
+)
+
+const CategoryList = ({ title, categories, activeCategory }) => (
+  <div>
+    <ListTitle>{title}</ListTitle>
+    <List>
+      {categories.map(category =>
+        <Category
+          key={category.node.slug}
+          category={category.node}
+          active={category.node.slug === activeCategory}
+        />
+      )}
+    </List>
+  </div>
+)
+
+export default CategoryList
