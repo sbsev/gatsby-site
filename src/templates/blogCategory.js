1a922d8d30b0dced1d5a30ae40d8c13e77bc84a5
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-06-26 23:12:41 +0200

    feat: CategoryIndex comp

---
+++ src/templates/blogCategory.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import PostList from '../components/PostList'
+
+const CategoryIndex = props => (
+  <PostList {...props.data} />
+)
+
+export default CategoryIndex
+
+export const categories = graphql`
+  fragment categories on RootQueryType {
+    categories: allContentfulCategory(
+      sort: { fields: [title], order: ASC}
+    ) {
+      edges {
+        node {
+          title
+          slug
+        }
+      }
+    }
+  }
+`
+
+// postFields defined in src/templates/post.js
+export const pageQuery = graphql`
+  query BlogCategoryBySlug($slug: String!) {
+    site {
+      meta: siteMetadata {
+        title
+      }
+    }
+    posts: allContentfulPost(
+      sort: { fields: [ date ], order: DESC }
+      filter: { category: { slug: { eq: $slug } } }
+    ) {
+      edges {
+        node {
+          ...postFields
+        }
+      }
+    }
+    ...categories
+    activeCategory: contentfulCategory(slug: {eq: $slug}) {
+      slug
+    }
+  }
+`
