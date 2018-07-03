a4294ffbe30ab46a9fe772dc098e9a1ef17deecb
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-07-03 12:43:10 +0200

    style: images in PageBody

--- src/components/PageBody.js
+++ src/components/PageBody.js
@@ -1,29 +1,43 @@
 import styled from 'styled-components'
 
+const imageGrid = (props = {}) => `
+  display: grid;
+  grid-gap: ${props.gridGap || `2rem`};
+  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+  margin-top: 2rem;
+
+  h1, h2, h3, h4, h5, h6 {
+    grid-column: 1/-1;
+  }
+
+  p {
+    text-align: center;
+
+    img {
+      width: 100%;
+      border-radius: ${props.borderRadius || `0`};
+    }
+    em {
+      display: block;
+    }
+    strong {
+      display: block;
+      font-weight: 200;
+    }
+  }
+`
+
 export const PageBody = styled.article`
   #heads {
-    display: grid;
-    grid-gap: 2rem;
-    grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
-    margin-top: 2rem;
+    ${imageGrid({ borderRadius: `50%` })}
+  }
 
-    p {
-      text-align: center;
+  #partners {
+    ${imageGrid()}
+  }
 
-      img {
-        width: 100%;
-        max-width: 15rem;
-        border-radius: 50%;
-        margin: 0;
-      }
-      em {
-        display: block;
-      }
-      strong {
-        display: block;
-        font-weight: 200;
-      }
-    }
+  .side-by-side {
+    ${imageGrid({ gridGap: `1rem` })}
   }
 `
 
