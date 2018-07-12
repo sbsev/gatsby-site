19afd5290b6cac05f3ff1ec1de329d5616b39742
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-12 09:12:08 +0200

    feat: Breadcrumbs comp

---
+++ src/components/Breadcrumbs.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const Crumbs = styled.nav`
+  margin-bottom: 2rem;
+`
+
+const Breadcrumbs = ({ path }) => {
+  const crumbs = path.substring(1).split(`/`)
+  return <Crumbs>
+    {crumbs
+      .map((crumb, index) => {
+        const prevCrumbs = crumbs.slice(0, index + 1)
+        const link = `/` + prevCrumbs.join(`/`)
+        return <Link key={crumb} to={link}>{crumb.split(`-`).join(` `)}</Link>
+      })
+      .reduce((prev, curr) => [prev, ` / `, curr])}
+  </Crumbs>
+}
+
+export default Breadcrumbs
