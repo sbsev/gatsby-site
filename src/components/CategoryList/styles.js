a4014a6ce60c566ca809a2d363d93bf0bab1b286
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-12 09:18:57 +0200

    style: CategoryList, add category icons

--- src/components/CategoryList/styles.js
+++ src/components/CategoryList/styles.js
@@ -3,15 +3,25 @@ import Link from 'gatsby-link'
 
 import mediaQuery from '../../utils/mediaQuery'
 
-export const CategoryLink = styled(Link)`
-  white-space: nowrap;
-  color: ${props => props.active ? props.theme.mainGreen : ''}
+export const CategoryIcon = styled.img`
+  height: 1.75rem;
+  width: 1.75rem;
+  margin: 0 1rem 0 0;
+  background: ${props => props.theme.lightGray};
+  border-radius: ${props => props.theme.mediumBorderRadius};
+  padding: 0.2rem;
 `
 
-export const List = styled.ul`
-  ${mediaQuery.phone} {
-    columns: 2;
-  }
+export const CategoryLink = styled(Link)`
+  display: flex;
+  white-space: nowrap;
+  color: ${props => props.active ? props.theme.mainGreen : ''};
+`
+
+export const List = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr));
+  grid-gap: 1rem;
 `
 
 export const ListTitle = styled.h1`
