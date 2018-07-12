dd20874b42a7dc695c4c91dadc185860f68a1238
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-12 09:17:04 +0200

    feat: gatsby-node.js create pages for wiki articles, sections and subsections

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,8 +3,11 @@ const path = require('path')
 const pageTemplate = path.resolve('./src/templates/page.js')
 const postTemplate = path.resolve('./src/templates/post.js')
 const blogCategoryTemplate = path.resolve('./src/templates/blogCategory.js')
+const wikiSubsectionTemplate = path.resolve('./src/templates/wikiSubsection.js')
+const wikiSectionTemplate = path.resolve('./src/templates/wikiSection.js')
+const wikiArticleTemplate = path.resolve('./src/templates/wikiArticle.js')
 
-const contentfulQuery = contentType => `
+const contentfulQuery = (contentType, fragment = ``) => `
   {
     content: allContentful${contentType} {
       edges {
@@ -13,24 +16,55 @@ const contentfulQuery = contentType => `
             id
           }
           slug
+          ${fragment}
         }
       }
     }
   }
 `
 
+const wikiSubsectionFragment = `
+  sections: wiki_section {
+    slug
+  }
+`
+
+const wikiArticleFragment = `
+  section {
+    slug
+  }
+  subsection {
+    slug
+  }
+`
+
 const pageSets = [
   { query: contentfulQuery(`Page`), component: pageTemplate },
   { query: contentfulQuery(`Post`), component: postTemplate },
-  { query: contentfulQuery(`Category`), component: blogCategoryTemplate },
+  { query: contentfulQuery(`BlogCategory`), component: blogCategoryTemplate },
+  { query: contentfulQuery(`WikiSection`), component: wikiSectionTemplate },
+  {
+    query: contentfulQuery(`WikiSubsection`, wikiSubsectionFragment),
+    component: wikiSubsectionTemplate
+  },
+    {
+    query: contentfulQuery(`WikiArticle`, wikiArticleFragment),
+    component: wikiArticleTemplate
+  },
 ]
 
 const pagePath = node => {
   switch (node.parent.id) {
     case `Post`:
+    case `Blog Category`:
       return `/blog/` + node.slug
-    case `Category`:
-      return `/blog/category/` + node.slug
+    case `Wiki Section`:
+      return `/wiki/${node.slug}`
+    case `Wiki Subsection`:
+      return `/wiki/${node.sections[0].slug}/${node.slug}`
+    case `Wiki Article`:
+      if (!node.subsection) return `/wiki/${node.section.slug}/${node.slug}`
+      return `/wiki/${node.section.slug}/${node.subsection.slug}/${node.slug}`
     default:
       return node.slug
   }
