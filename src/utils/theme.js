adf3522650f7755c2887237256a948f3db83cd38
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-12 09:23:00 +0200

    syle: modify theme

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -3,15 +3,17 @@ const theme = {
   darkBlue: `#062f4f`,
   mainGray: `#464849`,
   lightGray: `#d8d8d8`,
+  veryLightGray: `#efefef`,
   darkGray: `#2a2c30`,
   mainWhite: `white`,
   mainGreen: `#00a35b`,
+  lightGreen: `#60ffb5`,
 
   maxWidth: `50rem`,
 
-  smallBorder: `1px`,
-  mediumBorder: `2px`,
-  largeBorder: `4px`,
+  smallBorder: `0.1rem`,
+  mediumBorder: `0.2rem`,
+  largeBorder: `0.4rem`,
   
   smallBorderRadius: `0.2rem`,
   mediumBorderRadius: `0.5rem`,
