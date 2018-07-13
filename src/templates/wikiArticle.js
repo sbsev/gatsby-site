ea067fbc465a69cb0fea1c2c11c78c9f1688aebe
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-13 14:47:59 +0200

    refactor: wikiArticle template

--- src/templates/wikiArticle.js
+++ src/templates/wikiArticle.js
@@ -1,30 +1,22 @@
 import React, { Fragment } from 'react'
-import Helmet from 'react-helmet'
 
+import Helmet from '../components/Helmet'
 import Breadcrumbs from '../components/Breadcrumbs'
 import PageTitle from '../components/PageTitle'
 import PageBody from '../components/PageBody'
 import PageMeta from '../components/PageMeta'
 
-const WikiArticleTemplate = props => {
-  const { article, site } = props.data
-  const { title, slug, section, subsection, body } = article
+const WikiArticleTemplate = ({ data, location }) => {
+  const { article, site } = data
+  const { title: { title }, body } = article
+  const { html, excerpt } = body.data
+  const path = location.pathname
   return (
     <Fragment>
-      <Helmet>
-        <title>{`${title.title} | ${site.meta.title}`}</title>
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title.title} />
-        <meta
-          property="og:url"
-          content={`${site.meta.url}/articles/${slug}`}
-        />
-        <meta property="og:description" content={body.data.excerpt} />
-        <meta name="description" content={body.data.excerpt} />
-      </Helmet>
-      <Breadcrumbs path={props.location.pathname} />
-      <PageTitle text={title.title} />
-      {body && <PageBody dangerouslySetInnerHTML={{__html: body.data.html}} />}
+      <Helmet pageTitle={title} site={site} path={path} description={excerpt} />
+      <Breadcrumbs path={path} />
+      <PageTitle text={title} />
+      <PageBody dangerouslySetInnerHTML={{__html: html}} />
       <PageMeta {...article} />
     </Fragment>
   )
@@ -32,42 +24,44 @@ const WikiArticleTemplate = props => {
 
 export default WikiArticleTemplate
 
-export const wikiArticleQuery = graphql`
-  query WikiArticleBySlug($slug: String!) {
-    site {
-      meta: siteMetadata {
-        title
-        url: siteUrl
-      }
+export const articleFields = graphql`
+  fragment articleFields on ContentfulWikiArticle {
+    title {
+      title
     }
-    article: contentfulWikiArticle(slug: {eq: $slug}) {
-      title {
-        title
-      }
-      slug
-      body {
-        data: childMarkdownRemark {
-          html
-          headings {
-            value
-            depth
-          }
+    slug
+    body {
+      data: childMarkdownRemark {
+        html
+        excerpt
+        headings {
+          value
+          depth
         }
       }
-      created: createdAt(formatString: "D. MMMM YYYY", locale: "de")
-      updated: updatedAt(formatString: "D. MMMM YYYY", locale: "de")
-      authors: author {
-        name
-        email
-      }
-      section {
-        title
-        slug
-      }
-      subsection {
-        title
-        slug
-      }
+    }
+    created: createdAt(formatString: "D. MMMM YYYY", locale: "de")
+    updated: updatedAt(formatString: "D. MMMM YYYY", locale: "de")
+    authors: author {
+      name
+      email
+    }
+    section {
+      title
+      slug
+    }
+    subsection {
+      title
+      slug
+    }
+  }
+`
+
+export const wikiArticleQuery = graphql`
+  query WikiArticleBySlug($slug: String!) {
+    ...siteMetaQuery
+    article: contentfulWikiArticle(slug: {eq: $slug}) {
+      ...articleFields
     }
   }
 `
