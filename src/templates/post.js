e869ff9352b40ddf35dc298559f08e4fb19df506
Janosh Riebesell <Janosh@Janoshs-iMac.fritz.box>
2018-07-13 14:47:49 +0200

    refactor: post template

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,28 +1,22 @@
 import React, { Fragment } from 'react'
-import Helmet from 'react-helmet'
 
+import Helmet from '../components/Helmet'
 import PostTitle from '../components/PostTitle'
 import FeaturedImage from '../components/FeaturedImage'
 
-const PostTemplate = props => {
-  const { post, site } = props.data
+const PostTemplate = ({ data, location }) => {
+  const { post, site } = data
+  const { title: { title }, date, body, featuredImage } = post
+  const { timeToRead, html, excerpt } = body.data
+  const path = location.pathname
   return (
     <Fragment>
-      <Helmet>
-        <title>{`${post.title.title} | ${site.meta.title}`}</title>
-        <meta property="og:type" content="article" />
-        <meta property="og:title" content={post.title.title} />
-        <meta property="og:url" content={`${site.meta.url}/posts/${post.slug}`} />
-      </Helmet>
-      {post.featuredImage &&
-        <FeaturedImage src={post.featuredImage.file.url} alt={post.featuredImage.title} />
+      <Helmet pageTitle={title} site={site} path={path} description={excerpt} />
+      {featuredImage &&
+        <FeaturedImage src={featuredImage.file.url} alt={featuredImage.title} />
       }
-      <PostTitle
-        title={post.title.title}
-        date={post.date}
-        timeToRead={post.body.data.timeToRead}
-      />
-      <article dangerouslySetInnerHTML={{ __html: post.body.data.html }} />
+      <PostTitle title={title} date={date} timeToRead={timeToRead} />
+      <article dangerouslySetInnerHTML={{ __html: html }} />
     </Fragment>
   )
 }
@@ -61,12 +55,7 @@ export const postFields = graphql`
 
 export const postQuery = graphql`
   query PostBySlug($slug: String!) {
-    site {
-      meta: siteMetadata {
-        title
-        url: siteUrl
-      }
-    }
+    ...siteMetaQuery
     post: contentfulPost(slug: { eq: $slug }) {
       ...postFields
     }
