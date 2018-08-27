fb088d7797cd818eb0769d466e187b8423c0c244
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-08-27 13:29:57 +0200

    feat: remove typographyjs

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,11 +41,5 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
   ],
 }
