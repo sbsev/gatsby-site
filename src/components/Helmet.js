e8da1146ddbee4b2f167204404f1ea46cbd7f874
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-08-28 16:27:16 +0200

    feat: add PropTypes to Helmet and refactor

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -1,16 +1,32 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 const Head = ({ site, pageTitle, path, description }) => {
-  const { title: siteTitle, url } = site.meta
-  return <Helmet>
-    <title>{`${pageTitle} | ${siteTitle}`}</title>
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content={pageTitle} />
-    <meta property="og:url" content={url + path} />
-    {description && <meta property="og:description" content={description} />}
-    {description && <meta name="description" content={description} />}
-  </Helmet>
+  const title = pageTitle ? `${pageTitle} | ${site.title}` : site.title
+  const pageUrl = path ? site.url + path : site.url
+  const desc = description || site.description
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta property="og:type" content="website" />
+      {pageTitle && <meta property="og:title" content={pageTitle} />}
+      <meta property="og:url" content={pageUrl} />
+      <meta property="og:description" content={desc} />
+      <meta name="description" content={desc} />
+    </Helmet>
+  )
 }
 
 export default Head
+
+Head.propTypes = {
+  site: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }),
+  pageTitle: PropTypes.string,
+  path: PropTypes.string,
+  description: PropTypes.string,
+}
