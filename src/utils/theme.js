727f900a001c53e69af9f40612599f1160d5aca2
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-08-28 16:25:57 +0200

    feat: change and add theme colors

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,21 +1,26 @@
 const theme = {
-  mainBlue: `darkBlue`,
-  darkBlue: `#062f4f`,
+  mainBlue: `#003e90`,
+  darkBlue: `#003056`,
+  lightBlue: `#00aaff`,
+
+  mainGreen: `#6e9f23`,
+  darkGreen: `#008c65`,
+  lightGreen: `#00d69b`,
+
   mainGray: `#464849`,
   lightGray: `#d8d8d8`,
   veryLightGray: `#efefef`,
   darkGray: `#2a2c30`,
+
   mainWhite: `white`,
   mainBlack: `black`,
-  mainGreen: `#00a35b`,
-  lightGreen: `#60ffb5`,
 
   maxWidth: `50rem`,
 
   smallBorder: `0.1rem`,
-  mediumBorder: `0.2rem`,
-  largeBorder: `0.4rem`,
-  
+  mediumBorder: `0.15rem`,
+  largeBorder: `0.2rem`,
+
   smallBorderRadius: `0.2rem`,
   mediumBorderRadius: `0.5rem`,
   largeBorderRadius: `1rem`,
