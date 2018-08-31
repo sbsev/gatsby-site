3e6b027110e33f76d6197c9fd96373ae2fcde65b
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-08-31 21:37:56 +0200

    refactor: simplify PageMeta

--- src/components/PageMeta/index.js
+++ src/components/PageMeta/index.js
@@ -1,26 +1,31 @@
 import React from 'react'
 import _ from 'lodash'
 
-import { Layout, Content } from './styles'
+import { Container } from './styles'
 
 const PageMeta = ({ created, updated, authors }) => {
   authors = _.uniqWith(authors, _.isEqual)
-  return <Layout>
-    <Content>
-      Erstellt: {created} &bull;{` `}
-      {created !== updated && <span>Zuletzt bearbeitet: {updated} &bull; </span>}
-      {authors.length &&
-        <span>Autor{authors.length > 1 ? `en` : ``}: {
-          authors.map(author =>
-            <a key={author.name} href={`mailto:` + author.email}>
-              {author.name}
-            </a>
-          ).reduce((prev, curr) => [prev, `, `, curr])
-        }
+  return (
+    <Container>
+      Erstellt: {created} &bull;
+      {created !== updated && (
+        <span> Zuletzt bearbeitet: {updated} &bull; </span>
+      )}
+      {authors.length && (
+        <span>
+          Autor
+          {authors.length > 1 && `en`}:{' '}
+          {authors
+            .map(author => (
+              <a key={author.name} href={`mailto:` + author.email}>
+                {author.name}
+              </a>
+            ))
+            .reduce((prev, curr) => [prev, `, `, curr])}
         </span>
-      }
-    </Content>
-  </Layout>
+      )}
+    </Container>
+  )
 }
 
 export default PageMeta
