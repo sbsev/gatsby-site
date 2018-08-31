3714e8c31bb8f9ba064af57864b50cf962172cb6
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-08-31 21:35:28 +0200

    style: Social

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -2,19 +2,22 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import PropTypes from 'prop-types'
 
-import { Container, Icons } from './styles'
+import { Wrapper, Container, Toggle, Icons } from './styles'
 
-const Social = ({ social, iconSize, css }) => (
-  <Container>
-    {Object.keys(social).map(service => {
-      const Icon = Icons[service]
-      return (
-        <a href={social[service]}>
-          <Icon key={service} size={iconSize} css={css} />
-        </a>
-      )
-    })}
-  </Container>
+const Social = ({ social, iconSize, containerCss, iconCss, expandOnHover }) => (
+  <Wrapper>
+    {expandOnHover && <Toggle size={iconSize} css={iconCss} />}
+    <Container {...{ expandOnHover }} css={containerCss}>
+      {Object.keys(social).map(service => {
+        const Icon = Icons[service]
+        return (
+          <a key={service} href={social[service]}>
+            <Icon key={service} size={iconSize} css={iconCss} />
+          </a>
+        )
+      })}
+    </Container>
+  </Wrapper>
 )
 
 export default props => (
@@ -37,9 +40,7 @@ export default props => (
 )
 
 Social.propTypes = {
-  social: PropTypes.shape({
-    data: PropTypes.object.isRequired,
-  }).isRequired,
+  social: PropTypes.object.isRequired,
   iconSize: PropTypes.string.isRequired,
 }
 
--- src/components/Social/styles.js
+++ src/components/Social/styles.js
@@ -1,17 +1,54 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { Share } from 'styled-icons/material/Share'
 import { Email } from 'styled-icons/material/Email'
 import { Youtube } from 'styled-icons/fa-brands/Youtube'
 import { LinkedinIn as Linkedin } from 'styled-icons/fa-brands/LinkedinIn'
 import { FacebookF as Facebook } from 'styled-icons/fa-brands/FacebookF'
 import { Github } from 'styled-icons/fa-brands/Github'
 
-export const Container = styled.div`
-  display: grid;
+import mediaQuery from '../../utils/mediaQuery'
+
+export const Wrapper = styled.div`
+  position: relative;
+`
+
+const expandOnHover = css`
+  display: none;
+  position: absolute;
+  background: ${props => props.theme.lightGreen};
+  border-radius: ${props => props.theme.smallBorderRadius};
+  padding: 1vmin;
+  font-size: 1.8em;
+  ${Wrapper}:hover & {
+    display: grid;
+  }
+`
+
+const alwaysShow = css`
   grid-auto-flow: column;
-  grid-gap: 1.2vmin;
   align-items: end;
   grid-auto-columns: max-content;
-  justify-self: end;
+  grid-area: social;
+  ${props => props.css};
+`
+
+export const Container = styled.div`
+  display: grid;
+  grid-gap: 1.5vmin;
+  ${mediaQuery.minLaptop} {
+    ${alwaysShow};
+  }
+  ${mediaQuery.laptop} {
+    ${props => (props.expandOnHover ? expandOnHover : alwaysShow)};
+  }
+`
+
+export const Toggle = styled(Share)`
+  cursor: pointer;
+  font-size: 1.7em;
+  ${mediaQuery.minLaptop} {
+    display: none !important;
+  }
 `
 
 export const Icons = {
