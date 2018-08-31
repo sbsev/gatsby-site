b6fcf33bada92625a064e8d364211fec7557c94e
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-08-31 21:21:39 +0200

    feat: add favicon and React children to Helmet

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -2,7 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-const Head = ({ site, pageTitle, path, description }) => {
+import favicon from '../assets/favicon.png'
+
+const Head = ({ site, pageTitle, path, description, children }) => {
   const title = pageTitle ? `${pageTitle} | ${site.title}` : site.title
   const pageUrl = path ? site.url + path : site.url
   const desc = description || site.description
@@ -14,6 +16,8 @@ const Head = ({ site, pageTitle, path, description }) => {
       <meta property="og:url" content={pageUrl} />
       <meta property="og:description" content={desc} />
       <meta name="description" content={desc} />
+      <link rel="icon" type="image/png" href={favicon} />
+      {children}
     </Helmet>
   )
 }
