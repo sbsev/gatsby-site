352b140376c0fd358c2b5865f05390455f0382a0
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-08-31 21:08:57 +0200

    feat: add netBook mediaQuery

--- src/utils/mediaQuery.js
+++ src/utils/mediaQuery.js
@@ -4,7 +4,8 @@ const max = width => `@media only screen and (max-width: ${width}em)`
 export const screenSize = {
   // screen sizes in em units
   phone: 30,
-  tablet: 48,
+  tablet: 50,
+  netbook: 60,
   laptop: 70,
   desktop: 100,
 }
@@ -12,10 +13,12 @@ export const screenSize = {
 const mediaQuery = {
   phone: max(screenSize.phone),
   tablet: max(screenSize.tablet),
+  netbook: max(screenSize.netbook),
   laptop: max(screenSize.laptop),
   desktop: max(screenSize.desktop),
   minPhone: min(screenSize.phone),
   minTablet: min(screenSize.tablet),
+  minNetbook: min(screenSize.netbook),
   minLaptop: min(screenSize.laptop),
   minDesktop: min(screenSize.desktop),
 }
