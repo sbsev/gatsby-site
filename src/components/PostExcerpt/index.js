915b64be184cdc0991f82f023548ab1240d33c30
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-08-31 21:37:06 +0200

    feat: add FeaturedImage to PostExcerpt

--- src/components/PostExcerpt/index.js
+++ src/components/PostExcerpt/index.js
@@ -1,31 +1,31 @@
 import React from 'react'
 
-import { Article, Title, TitleLink, Meta, Tags, Tag } from './styles'
+import { Title, TitleLink, Meta, Tags, Tag, FeaturedImage } from './styles'
 import { DateIcon, TimeIcon } from '../Icons'
-// import FeaturedImage from '../FeaturedImage'
 
 const PostExcerpt = ({ post }) => (
-  <Article>
-    {/* {post.featuredImage &&
+  <article>
+    {post.featuredImage && (
       <FeaturedImage
         src={post.featuredImage.file.url}
         alt={post.featuredImage.title}
-        small smallMargin
       />
-    } */}
+    )}
     <Title>
-      <TitleLink to={'/blog/' + post.slug}>
-        {post.title.title}
-      </TitleLink>
+      <TitleLink to={'/blog/' + post.slug}>{post.title.title}</TitleLink>
     </Title>
     <Meta>
-      <DateIcon /> {post.date} | <TimeIcon /> {post.body.data.timeToRead} Min Lesezeit
+      <DateIcon /> {post.date} | <TimeIcon /> {post.body.data.timeToRead} Min
+      Lesezeit
     </Meta>
     <p dangerouslySetInnerHTML={{ __html: post.body.data.excerpt }} />
-    <Tags>Tags: {post.tags.map(tag =>
-      <Tag>{tag}</Tag>
-    )}</Tags>
-  </Article>
+    <Tags>
+      Tags:{' '}
+      {post.tags.map(tag => (
+        <Tag>{tag}</Tag>
+      ))}
+    </Tags>
+  </article>
 )
 
 export default PostExcerpt
--- src/components/PostExcerpt/styles.js
+++ src/components/PostExcerpt/styles.js
@@ -1,15 +1,9 @@
 import styled from 'styled-components'
 import { Link } from 'gatsby'
 
-export const Article = styled.article`
-  & + & {
-    margin-top: 3rem;
-  }
-`
-
 export const Title = styled.h1`
-  margin: 0.5rem 0;
-  font-size: 1.5rem;
+  margin: 0.5em 0;
+  font-size: 1.5em;
 `
 
 export const TitleLink = styled(Link)`
@@ -18,18 +12,26 @@ export const TitleLink = styled(Link)`
 `
 
 export const Meta = styled.div`
-  margin-bottom: 0.5rem;
-  font-size: 0.9rem;
+  margin-bottom: 0.5em;
+  font-size: 0.9em;
 `
 
-export const Tags = styled.p`
+export const Tags = styled.div`
   display: flex;
   flex-wrap: wrap;
+  font-size: 0.8em;
 `
 
 export const Tag = styled.span`
-  padding: 0 0.5rem;
-  border-radius: ${props => props.theme.largeBorderRadius};
+  padding: 0 0.5em;
+  border-radius: ${props => props.theme.smallBorderRadius};
   background: ${props => props.theme.lightGray};
-  margin-left: 0.5rem;
+  margin-left: 0.5em;
+`
+
+export const FeaturedImage = styled.img`
+  width: 50%;
+  transform: translate(50%);
+  max-height: 20vmax;
+  object-fit: cover;
 `
