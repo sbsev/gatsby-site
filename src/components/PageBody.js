d08c6267f87583d977b8d95d82889acf4ef29ad7
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-08-31 21:24:33 +0200

    chore: clean up PageBody

--- src/components/PageBody.js
+++ src/components/PageBody.js
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 
 const imageGrid = css => `
   display: grid;
-  grid-gap: 2rem;
-  grid-template-columns: repeat(auto-fit, minmax(5rem, 12rem));
-  margin: 2rem 0;
+  grid-gap: 2em;
+  grid-template-columns: repeat(auto-fit, minmax(5em, 12em));
+  margin: 2em 0;
 
   h1, h2, h3, h4, h5, h6 {
     grid-column: 1/-1;
@@ -26,42 +26,36 @@ const imageGrid = css => `
       font-weight: 200;
     }
   }
-
   ${css}
 `
 
 export const PageBody = styled.article`
-  #heads, #alumni {
+  #heads,
+  #alumni {
     ${imageGrid(`
       img {
         border-radius: 50%;
       }
-    `)}
+    `)};
   }
 
   #partners {
-    ${imageGrid()}
+    ${imageGrid()};
   }
 
   .side-by-side {
     display: grid;
-    grid-gap: 1rem;
-    margin: 2rem 0;
+    grid-gap: 1em;
+    margin: 2em 0;
     grid-auto-flow: column;
-    ${'' /* .gatsby-resp-image-link,
-    .gatsby-resp-image-wrapper,
-    .gatsby-resp-image-background-image,
-    .gatsby-resp-image-image,
-    img {
-      height: 100%;
-      object-fit: cover;
-    } */}
+    grid-auto-columns: minmax(10em, 40%);
   }
 
-  .multi-col-list ul, .multi-col-list ol {
+  .multi-col-list ul,
+  .multi-col-list ol {
     display: grid;
-    grid-gap: 0 2rem;
-    grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+    grid-gap: 0 2em;
+    grid-template-columns: repeat(auto-fit, minmax(10em, 1fr));
   }
 `
 
