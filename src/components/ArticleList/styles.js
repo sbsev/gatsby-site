df9c5cdbaef93cda87aa14aae98fa39a671ee582
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-08-31 21:40:47 +0200

    style: change rem to em units

--- src/components/ArticleList/styles.js
+++ src/components/ArticleList/styles.js
@@ -4,17 +4,17 @@ export const ArticleGrid = styled.div`
   display: grid;
   grid-auto-flow: column;
   grid-auto-columns: max-content;
-  grid-gap: 1rem;
+  grid-gap: 1em;
 `
 
 export const Article = styled.div`
   h4 {
-    padding: 0.2rem 0.5rem;
+    padding: 0.2em 0.5em;
     background: ${props => props.theme.lightGray};
     border-radius: ${props => props.theme.smallBorderRadius};
     color: ${props => props.theme.mainBlue};
   }
   h6 {
-    margin-top: 0.2rem;
+    margin-top: 0.2em;
   }
 `
