62c38586f05715f6ffe0cc0703e529fd9c7a3fea
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-08-31 21:07:33 +0200

    refactor: BreadCrumbs

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -3,21 +3,27 @@ import { Link } from 'gatsby'
 import styled from 'styled-components'
 
 const Crumbs = styled.nav`
-  margin-bottom: 2rem;
+  margin-bottom: 2em;
 `
 
 const Breadcrumbs = ({ path }) => {
   const crumbs = path.substring(1).split(`/`)
-  return <Crumbs>
-    <span>Pfad:&ensp;</span>
-    {crumbs
-      .map((crumb, index) => {
-        const prevCrumbs = crumbs.slice(0, index + 1)
-        const link = `/` + prevCrumbs.join(`/`)
-        return <Link key={crumb} to={link}>{crumb.split(`-`).join(` `)}</Link>
-      })
-      .reduce((prev, curr) => [prev, ` / `, curr])}
-  </Crumbs>
+  return (
+    <Crumbs>
+      <span>Pfad:&ensp;</span>
+      {crumbs
+        .map((crumb, index) => {
+          const prevCrumbs = crumbs.slice(0, index + 1)
+          const link = `/` + prevCrumbs.join(`/`)
+          return (
+            <Link key={crumb} to={link}>
+              {crumb.split(`-`).join(` `)}
+            </Link>
+          )
+        })
+        .reduce((prev, curr) => [prev, ` / `, curr])}
+    </Crumbs>
+  )
 }
 
 export default Breadcrumbs
