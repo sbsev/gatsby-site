4d461d2b33498121baecdd31bec0135ce279b4b4
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-09-02 12:57:19 +0200

    feat: add LandingPage using LandingGrid comp

---
+++ src/pages/index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+import LandingGrid from '../components/LandingGrid'
+import PageBody from '../components/PageBody'
+import PageMeta from '../components/PageMeta'
+
+const PageTemplate = ({ data, location }) => {
+  const { images, page } = data
+  const {
+    title: { title },
+    subtitle: { subtitle },
+    body,
+  } = page
+  const { excerpt, html } = body && body.data
+  return (
+    <Layout pageTitle={title} path={location.pathname} description={excerpt}>
+      <LandingGrid {...{ title, subtitle, images: images.edges }} />
+      {html && <PageBody dangerouslySetInnerHTML={{ __html: html }} />}
+      <PageMeta {...page} />
+    </Layout>
+  )
+}
+
+export default PageTemplate
+
+export const query = graphql`
+  {
+    page: contentfulPage(slug: { eq: "/" }) {
+      title {
+        title
+      }
+      subtitle {
+        subtitle
+      }
+      slug
+      body {
+        data: childMarkdownRemark {
+          excerpt
+          html
+        }
+      }
+      created: createdAt(formatString: "D. MMMM YYYY", locale: "de")
+      updated: updatedAt(formatString: "D. MMMM YYYY", locale: "de")
+      authors: author {
+        name
+        email
+      }
+    }
+    images: allContentfulAsset(
+      filter: { file: { fileName: { regex: "/stock/" } } }
+    ) {
+      edges {
+        node {
+          title
+          fluid {
+            src
+          }
+        }
+      }
+    }
+  }
+`
