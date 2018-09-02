c49cfac6a70ff5e4b201828c51090b1f7903c56c
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-09-02 19:16:51 +0200

    feat: adjust min and max font size

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,8 @@
 const typography = {
   fonts: `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif`,
   // font sizes and line heigths in em units
-  minFontSize: 0.8,
-  maxFontSize: 1.2,
+  minFontSize: 1,
+  maxFontSize: 1.3,
   minLineHeight: 1.5,
   maxLineHeight: 2,
 }
