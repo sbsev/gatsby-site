8c741fbc51c652907ccc29014194374cea47b8ed
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-09-04 11:12:40 +0200

    feat: add react-headroom to Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Headroom from 'react-headroom'
 
 import { Container, Logo, SiteTitle } from './styles'
 import Nav from '../Nav'
@@ -6,14 +7,16 @@ import Social from '../Social'
 import { navLinkStyle } from '../Nav/styles'
 
 const Header = ({ site }) => (
-  <Container>
-    <SiteTitle to="/" title={site.title} rel="home" styles={navLinkStyle}>
-      <Logo />
-      {site.title}
-    </SiteTitle>
-    <Nav />
-    <Social expandOnHover iconCss={navLinkStyle} />
-  </Container>
+  <Headroom>
+    <Container>
+      <SiteTitle to="/" title={site.title} rel="home" styles={navLinkStyle}>
+        <Logo />
+        {site.title}
+      </SiteTitle>
+      <Nav />
+      <Social expandOnHover iconCss={navLinkStyle} />
+    </Container>
+  </Headroom>
 )
 
 export default Header
