f2fdb136c20fae7eb13a462331f8ea6fe4cc83cf
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-09-16 17:02:21 +0200

    feat: add Logo to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 import PageTitle from '../components/PageTitle'
+import Logo from '../assets/logo'
 import PageBody from '../components/styles/PageBody'
 import PageMeta from '../components/PageMeta'
 
@@ -14,10 +15,11 @@ const PageNotFound = ({ data: { page }, location }) => {
   const { excerpt, html } = body && body.data
   return (
     <Layout pageTitle={title} path={location.pathname} description={excerpt}>
-      <PageTitle text={title} />
-      {html && (
-        <PageBody isLanding dangerouslySetInnerHTML={{ __html: html }} />
-      )}
+      <PageTitle>
+        <Logo width="calc(5em + 5vw)" />
+        <h1>{title}</h1>
+      </PageTitle>
+      {html && <PageBody dangerouslySetInnerHTML={{ __html: html }} />}
       <PageMeta {...page} />
     </Layout>
   )
