5f911e3ac1b110ccc1719d5e551a60f39a5bfa23
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-09-21 11:08:13 +0200

    feat: add source and poweredBy to Footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import mediaQuery from '../../utils/mediaQuery'
 import Link from '../Link'
 
-export const Container = styled.div`
+export const FooterContainer = styled.div`
   background: ${props => props.theme.darkBlue};
   padding: 5vmin;
   color: ${props => props.theme.lightBlue};
@@ -11,7 +11,7 @@ export const Container = styled.div`
   text-align: center;
   align-items: center;
   justify-items: center;
-  grid-gap: 3vmin;
+  grid-gap: 5vh 3vw;
   grid-template-areas:
     'social'
     'copyright'
@@ -44,4 +44,21 @@ export const FooterLinks = styled.div`
 
 export const FooterLink = styled(Link)`
   ${props => props.css};
+`
+
+export const Source = styled.span`
+  a {
+    color: ${props => props.theme.lightGreen};
+  }
+`
+
+export const PoweredBy = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: max-content;
+  grid-gap: 0.5em;
+  a {
+    height: 1.5em;
+    width: 1.5em;
+  }
 `
