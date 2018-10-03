69017acc32a1bc99b46c5ea154d6c2a3ee5d0804
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-10-03 16:04:01 +0100

    feat: add posts query for index building in algolia.js

--- src/utils/algolia.js
+++ src/utils/algolia.js
@@ -1,9 +1,8 @@
-
 const pageQuery = `{
   pages: allContentfulPage {
     edges {
       node {
-        objectID:id
+        id
         slug
         title {
           title
@@ -22,11 +21,64 @@ const pageQuery = `{
   }
 }`
 
+const postQuery = `{
+  posts: allContentfulPost {
+    edges {
+      node {
+        id
+        slug
+        title {
+          title
+        }
+        date(formatString: "D. MMMM YYYY", locale: "de")
+        author {
+          name
+          email
+          homepage
+        }
+        categories {
+          title
+          slug
+        }
+        body {
+          data: childMarkdownRemark {
+            excerpt
+            headings {
+              value
+              depth
+            }
+          }
+        }
+      }
+    }
+  }
+}`
+
 const queries = [
   {
     query: pageQuery,
-    transformer: ({ data }) => data.pages.edges.map(({ node }) => node),
+    transformer: ({ data }) =>
+      data.pages.edges.map(({ node }) => ({
+        title: node.title.title,
+        slug: node.slug,
+        ...node.body.data,
+        objectID: node.id,
+      })),
     indexName: `Pages`,
+  },
+  {
+    query: postQuery,
+    transformer: ({ data }) =>
+      data.posts.edges.map(({ node }) => ({
+        title: node.title.title,
+        slug: node.slug,
+        date: node.date,
+        author: node.author,
+        categories: node.categories,
+        ...node.body.data,
+        objectID: node.id,
+      })),
+    indexName: `Posts`,
   },
 ]
 
