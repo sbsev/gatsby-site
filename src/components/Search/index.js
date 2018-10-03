f62d01e04c0b6fa500abeeca333f35d038c3aa37
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-10-03 16:05:56 +0100

    feat: add posts to Search results

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,62 +1,85 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import algoliasearch from 'algoliasearch/lite'
-import {
-  InstantSearch,
-  SearchBox,
-  InfiniteHits,
-  Highlight,
-  PoweredBy,
-} from 'react-instantsearch-dom'
-import { Link } from 'gatsby'
+import { InstantSearch, SearchBox, Index, Hits } from 'react-instantsearch-dom'
+import { Algolia } from 'styled-icons/fa-brands/Algolia'
 
-import { Container, SearchWrapper, HitsWrapper, Loupe } from './styles'
+import { Root, HitsWrapper, Input, Loupe, By } from './styles'
+import PageHit from './PageHit'
+import PostHit from './PostHit'
 
 const searchClient = algoliasearch(
   'T0ZLKGU1XK',
   '2bba2dc22c305d8a0472c4a76690093e'
 )
 
-const Hit = ({ hit }) => (
-  <div>
-    <Link to={hit.slug}>
-      <h3>
-        <Highlight attribute="title.title" hit={hit} />
-      </h3>
-    </Link>
-    <Highlight attribute="body.data.excerpt" hit={hit} />
-  </div>
-)
+const events = ['mousedown', 'touchstart']
 
 export default class Search extends Component {
-  state = { query: ``, showInput: false }
+  state = { query: ``, showHits: false }
 
-  stateChange = state => {
+  updateState = state => {
     this.setState(state)
   }
 
-  toggleInput = () => {
-    this.setState({ showInput: !this.state.showInput })
-    console.log('state :', this.state)
+  enableHits = () => {
+    this.setState({ showHits: true })
+  }
+
+  handleClickOutside = event => {
+    if (this.node) {
+      const node = ReactDOM.findDOMNode(this.node)
+      if (!node.contains(event.target)) {
+        this.setState({ showHits: false })
+      }
+    }
+  }
+
+  componentDidMount() {
+    events.forEach(event =>
+      document.addEventListener(event, this.handleClickOutside)
+    )
+  }
+
+  componentWillUnmount() {
+    events.forEach(event =>
+      document.removeEventListener(event, this.handleClickOutside)
+    )
   }
 
   render() {
     return (
-      <Container>
-        <Loupe onClick={this.toggleInput} />
-        <SearchWrapper show={this.state.showInput}>
-          <InstantSearch
-            indexName="Pages"
-            searchClient={searchClient}
-            onSearchStateChange={this.stateChange}
-          >
-            <SearchBox translations={{ placeholder: `Suche` }} />
-            <HitsWrapper show={this.state.query.length > 0}>
-              <InfiniteHits hitComponent={Hit} />
-              <PoweredBy />
-            </HitsWrapper>
-          </InstantSearch>
-        </SearchWrapper>
-      </Container>
+      <InstantSearch
+        indexName="Pages"
+        searchClient={searchClient}
+        onSearchStateChange={this.updateState}
+        root={{ Root }}
+        ref={node => (this.node = node)}
+      >
+        <Input>
+          <SearchBox
+            translations={{ placeholder: `Suche` }}
+            onFocus={this.enableHits}
+          />
+          <Loupe />
+        </Input>
+        <HitsWrapper show={this.state.query.length > 0 && this.state.showHits}>
+          <Index indexName="Pages">
+            <h2>Seiten</h2>
+            <Hits hitComponent={PageHit} />
+          </Index>
+          <Index indexName="Posts">
+            <h2>Blog Posts</h2>
+            <Hits hitComponent={PostHit} />
+          </Index>
+          <By>
+            Powered by{' '}
+            <a href="https://www.algolia.com">
+              <Algolia size="1em" /> Algolia
+            </a>
+          </By>
+        </HitsWrapper>
+      </InstantSearch>
     )
   }
 }
