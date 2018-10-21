8d1a03ecda0c9886e45831f20cdb7fbd5f0d999f
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-10-21 14:24:25 +0100

    fix: make subsection optional in ArticleList

--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -9,18 +9,21 @@ const ArticleList = ({ articles }) => (
     <ArticleGrid>
       {articles.edges.map(({ node: article }) => {
         const { section, subsection, title, slug, body } = article
-        const link = `/wiki/${section.slug}/${subsection.slug}/${slug}`
-        return <Article key={title.title}>
-          <Link to={link}>
-            <h4>{title.title}</h4>
-          </Link>
-          <h6>Inhaltsverzeichnis</h6>
-          <ul>
-            {body.data.headings.map(heading =>
-              <li key={heading.value}>{heading.value}</li>
-            )}
-          </ul>
-        </Article>
+        const link = `/wiki/${section.slug}${subsection &&
+          `/` + subsection.slug}/${slug}`
+        return (
+          <Article key={title.title}>
+            <Link to={link}>
+              <h4>{title.title}</h4>
+            </Link>
+            <h6>Inhaltsverzeichnis</h6>
+            <ul>
+              {body.data.headings.map(heading => (
+                <li key={heading.value}>{heading.value}</li>
+              ))}
+            </ul>
+          </Article>
+        )
       })}
     </ArticleGrid>
   </Fragment>
