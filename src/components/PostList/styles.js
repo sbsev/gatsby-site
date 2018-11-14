425d283c437cf2dc0e6ba5cd17b508bd1e2182e4
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-11-14 17:08:06 +0000

    feat: render blog excerpts in masonry layout

--- src/components/PostList/styles.js
+++ src/components/PostList/styles.js
@@ -2,5 +2,16 @@ import styled from 'styled-components'
 
 export const Posts = styled.div`
   display: grid;
-  grid-gap: 10vmin;
+  grid-template-columns: repeat(auto-fit, minmax(15em, 1fr));
+  grid-auto-rows: calc(${props => props.rowHeight}px - 2em);
+  grid-gap: 2em;
+  font-size: 0.8em;
+  line-height: 1.4em;
+  grid-column: 2/4;
+`
+
+export const Post = styled.div`
+  grid-row: span ${props => props.span};
+  display: flex;
+  flex-direction: column;
 `
