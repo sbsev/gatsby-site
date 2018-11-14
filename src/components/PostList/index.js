0693b55ee67f5f449c2f8897b83c252175ddaf30
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-11-14 17:08:06 +0000

    feat: render blog excerpts in masonry layout

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,20 +1,32 @@
-import React, { Fragment } from 'react'
+import React, { Component } from 'react'
 
-import { Posts } from './styles'
-import CategoryList from '../CategoryList'
+import { Posts, Post } from './styles'
 import PostExcerpt from '../PostExcerpt'
 
-const PostList = ({ posts, categories }) => (
-  <Fragment>
-    <CategoryList title="Kategorien" categories={categories.edges} />
-    {posts && (
-      <Posts>
-        {posts.edges.map(({ node }) => (
-          <PostExcerpt key={node.slug} post={node} />
+export default class PostList extends Component {
+  state = { spans: [], rowHeight: 50 }
+
+  componentDidMount() {
+    const posts = document.getElementById('posts')
+    const spans = []
+    Array.from(posts.children).forEach(child => {
+      spans.push(
+        Math.ceil(child.children[0].clientHeight / this.state.rowHeight) + 1
+      )
+    })
+    this.setState({ spans })
+  }
+
+  render() {
+    const { rowHeight, spans } = this.state
+    return (
+      <Posts id="posts" rowHeight={rowHeight}>
+        {this.props.posts.map(({ node }, i) => (
+          <Post key={node.slug} span={spans[i]}>
+            <PostExcerpt post={node} />
+          </Post>
         ))}
       </Posts>
-    )}
-  </Fragment>
-)
-
-export default PostList
+    )
+  }
+}
