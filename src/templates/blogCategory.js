55799d348a4baed9258ecda92e303a587134e18e
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-11-14 17:06:35 +0000

    feat: absorb blog page into blogCategory template 'Alle'

--- src/templates/blogCategory.js
+++ src/templates/blogCategory.js
@@ -3,31 +3,56 @@ import { graphql } from 'gatsby'
 
 import Global from '../components/Global'
 import PageTitle from '../components/PageTitle'
+import CategoryList from '../components/CategoryList'
 import PostList from '../components/PostList'
 
-const blogCategoryTemplate = ({ data, location }) => {
-  const { activeCategory = { title: ``, description: { text: `` } } } = data
-  const title = `Blog - ${activeCategory.title}`
-  const { text } = activeCategory.description
+const CategoryTemplate = ({ data, location, title }) => {
+  const { active, categories } = data
+  let { posts } = data
   const path = location.pathname
+  if (active.slug !== `/`) {
+    posts.edges = posts.edges.filter(({ node }) =>
+      node.categories.map(category => category.slug).includes(active.slug)
+    )
+  }
   return (
-    <Global pageTitle={title} path={path} description={text}>
+    <Global pageTitle={title} path={path} description={active.description.text}>
       <PageTitle>
         <h1>{title}</h1>
       </PageTitle>
-      <PostList {...data} />
+      <CategoryList categories={categories.edges} />
+      {posts && <PostList posts={posts.edges} />}
     </Global>
   )
 }
 
-export default blogCategoryTemplate
+export default CategoryTemplate
+
+CategoryTemplate.defaultProps = {
+  title: `Blog`,
+}
 
 export const query = graphql`
-  query($slug: String!) {
-    posts: allContentfulPost(
-      sort: { fields: [date], order: DESC }
-      filter: { categories: { elemMatch: { slug: { eq: $slug } } } }
+  fragment categories on Query {
+    categories: allContentfulBlogCategory(
+      sort: { fields: [title], order: ASC }
     ) {
+      edges {
+        node {
+          title
+          slug
+          icon {
+            title
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+  query($slug: String!) {
+    posts: allContentfulPost(sort: { fields: [date], order: DESC }) {
       edges {
         node {
           ...postFields
@@ -35,7 +60,7 @@ export const query = graphql`
       }
     }
     ...categories
-    activeCategory: contentfulBlogCategory(slug: { eq: $slug }) {
+    active: contentfulBlogCategory(slug: { eq: $slug }) {
       title
       slug
       description {
