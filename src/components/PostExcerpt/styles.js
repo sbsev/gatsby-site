275d26ff142e7907596f562e15f2959cc60d85d5
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-11-16 20:45:10 +0000

    refactor: PostExcerpt

--- src/components/PostExcerpt/styles.js
+++ src/components/PostExcerpt/styles.js
@@ -1,17 +1,21 @@
 import styled from 'styled-components'
-import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 
 export const Post = styled.article`
+  height: 100%;
+  display: grid;
   background: ${props => props.theme.veryLightGray};
-  padding: 1em;
   border-radius: ${props => props.theme.mediumBorderRadius};
-  flex: 1;
-  align-items: space-between;
+  border: 1px solid ${props => props.theme.lightGray};
+  overflow: scroll;
+  > div {
+    padding: 1em;
+    display: grid;
+  }
 `
 
 export const Title = styled.h3`
-  margin: 0.5em 0;
+  margin: 0;
 `
 
 export const Meta = styled.div`
@@ -21,31 +25,11 @@ export const Meta = styled.div`
     'photo author'
     'photo date'
     'photo readtime';
-  grid-auto-columns: max-content;
+  grid-template-columns: max-content 1fr;
+  margin-bottom: 0.5em;
 `
 
 export const AuthorPhoto = styled(Img)`
   border-radius: 50%;
   grid-area: photo;
-`
-
-export const Categories = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  padding: 0.5em 0;
-`
-
-export const Category = styled(Link)`
-  padding: 0 0.5em;
-  border-radius: ${props => props.theme.smallBorderRadius};
-  background: ${props => props.theme.lightGray};
-  margin-left: 0.5em;
-`
-
-export const FeaturedImage = styled(Img)`
-  max-height: 25vmax;
-  margin: -1em;
-  margin-bottom: initial;
-  border-top-left-radius: ${props => props.theme.mediumBorderRadius};
-  border-top-right-radius: ${props => props.theme.mediumBorderRadius};
 `
