31dcef6ea5cf5144c7f0da55d130467d58eba948
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-11-16 20:44:24 +0000

    feat: PageTitle enable dynamic width border-bottom

--- src/components/PageTitle/index.js
+++ src/components/PageTitle/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 import { Title } from './styles'
 
-const PageTitle = ({ children }) => <Title>{children}</Title>
+const PageTitle = ({ children, cols }) => <Title cols={cols}>{children}</Title>
 
 export default PageTitle
