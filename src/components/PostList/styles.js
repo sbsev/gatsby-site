2bbeeb232cc43800ce2e803b65368d54a787de9f
JanoshRiebesell <janosh.riebesell@gmail.com>
2018-11-18 13:54:04 +0000

    style: center PostList on mobile

--- src/components/PostList/styles.js
+++ src/components/PostList/styles.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
+import mediaQuery from '../../utils/mediaQuery'
 
 export const Container = styled.div`
   font-size: 0.8em;
   line-height: 1.4em;
   grid-column: 2/4;
+  ${mediaQuery.phone} {
+    grid-column: 3;
+  }
 `
