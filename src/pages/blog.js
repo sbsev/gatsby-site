111ea4155030544878c0e292be99233dbd9a06fa
JanoshRiebesell <janosh.riebesell@gmail.com>
2019-01-26 20:14:28 +0000

    feat: create new blog page from former blog category template

---
+++ src/pages/blog.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react"
+import { graphql } from "gatsby"
+
+import Global from "../components/Global"
+import PageTitle from "../components/PageTitle"
+import TagList from "../components/TagList"
+import PostList from "../components/PostList"
+
+const TagTemplate = ({ data, location, title = `Blog` }) => {
+  const { posts, tags } = data
+  const [tag, setTag] = useState(`Alle`)
+  const filteredPosts =
+    tag === `Alle`
+      ? posts.edges
+      : posts.edges.filter(({ node }) =>
+        node.tags.map(tag => tag.title).includes(tag)
+      )
+  return (
+    <Global pageTitle={title} path={location.pathname}>
+      <PageTitle cols="2 / -2">
+        <h1>{title}</h1>
+      </PageTitle>
+      <TagList tags={tags.edges} activeTag={tag} setTag={setTag} />
+      {posts && (
+        <PostList posts={filteredPosts} activeTag={tag} setTag={setTag} />
+      )}
+    </Global>
+  )
+}
+
+export default TagTemplate
+
+export const query = graphql`
+  {
+    posts: allContentfulPost(sort: { fields: [date], order: DESC }) {
+      edges {
+        node {
+          ...postFields
+        }
+      }
+    }
+    tags: allContentfulBlogTag(sort: { fields: [title], order: ASC }) {
+      edges {
+        node {
+          title
+          slug
+          icon {
+            title
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
