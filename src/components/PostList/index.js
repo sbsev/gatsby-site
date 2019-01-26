1a6808fcff073661d910c4a1f509b62f5f67d148
JanoshRiebesell <janosh.riebesell@gmail.com>
2019-01-26 20:12:04 +0000

    feat: PostList pass props through to PostExcerpt

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react"
 
-import Masonry from '../Masonry'
-import PostExcerpt from '../PostExcerpt'
-import { Container } from './styles'
+import Masonry from "../Masonry"
+import PostExcerpt from "../PostExcerpt"
+import { Container } from "./styles"
 
-const PostList = ({ posts }) => (
+const PostList = ({ posts, ...rest }) => (
   <Container>
     <Masonry>
       {posts.map(({ node }) => (
-        <PostExcerpt key={node.slug} post={node} />
+        <PostExcerpt key={node.slug} post={node} {...rest} />
       ))}
     </Masonry>
   </Container>
