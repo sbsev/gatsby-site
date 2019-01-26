a4a8e2f4ea8333de044b6e19a3303a476eedc111
JanoshRiebesell <janosh.riebesell@gmail.com>
2019-01-26 20:11:10 +0000

    feat: add Tag button to PostExcerpt

--- src/components/PostExcerpt/index.js
+++ src/components/PostExcerpt/index.js
@@ -1,59 +1,61 @@
-import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
-import Img from 'gatsby-image'
+import React, { Fragment } from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import Img from "gatsby-image"
 
-import { UserEdit } from 'styled-icons/fa-solid/UserEdit'
-import { Email } from 'styled-icons/material/Email'
-import { ExternalLinkAlt } from 'styled-icons/fa-solid/ExternalLinkAlt'
-import { Calendar } from 'styled-icons/octicons/Calendar'
-import { Timer } from 'styled-icons/material/Timer'
+import { UserEdit } from "styled-icons/fa-solid/UserEdit"
+import { Email } from "styled-icons/material/Email"
+import { ExternalLinkAlt } from "styled-icons/fa-solid/ExternalLinkAlt"
+import { Calendar } from "styled-icons/octicons/Calendar"
+import { Timer } from "styled-icons/material/Timer"
 
-import { Post, Title, Meta, AuthorPhoto } from './styles'
+import { Post, Title, Meta, AuthorPhoto, Tag } from "./styles"
 
-const PostExcerpt = ({ post, iconSize }) => {
-  const { featuredImage, slug, title, author, date, categories, body } = post
+const PostExcerpt = ({ post, activeTag, setTag }) => {
+  const { cover, slug, title, author, date, tags, body } = post
   const { timeToRead, excerpt } = body.data
   return (
     <Post>
-      {featuredImage && (
-        <Link to={`blog/` + slug}>
-          <Img fluid={featuredImage.fluid} alt={featuredImage.title} />
+      {cover && cover.fluid && (
+        <Link to={`/blog/` + slug}>
+          <Img fluid={cover.fluid} alt={cover.title} />
         </Link>
       )}
       <main>
         <Title>
-          <Link to={`blog/` + slug}>{title}</Link>
+          <Link to={`/blog/` + slug}>{title}</Link>
         </Title>
         <Meta>
           <AuthorPhoto fixed={author.photo.fixed} alt={author.name} />
           <div>
-            <UserEdit size={iconSize} /> &nbsp;{author.name}
+            <UserEdit size="1em" /> &nbsp;{author.name}
             {author.homepage && (
               <a href={author.homepage}>
                 &nbsp;
-                <ExternalLinkAlt size={iconSize} />
+                <ExternalLinkAlt size="1em" />
               </a>
             )}
             {author.email && (
               <a href={`mailto:${author.email}`}>
-                &nbsp; <Email size={iconSize} />
+                &nbsp; <Email size="1em" />
               </a>
             )}
           </div>
           <div>
-            <Calendar size={iconSize} /> &nbsp;{date}
+            <Calendar size="1em" /> &nbsp;{date}
           </div>
           <div>
-            <Timer size={iconSize} /> &nbsp;{timeToRead} Min Lesezeit
+            <Timer size="1em" /> &nbsp;{timeToRead} Min Lesezeit
           </div>
         </Meta>
         <div>
-          <span>Kategorien: </span>
-          {categories.map((category, index) => (
-            <Fragment key={category.slug}>
+          <span>Tags: </span>
+          {tags.map(({ title }, index) => (
+            <Fragment key={title}>
               {!!index && `, `}
-              <Link to={`blog/` + category.slug}>{category.title}</Link>
+              <Tag active={activeTag === title} onClick={() => setTag(title)}>
+                {title}
+              </Tag>
             </Fragment>
           ))}
         </div>
@@ -67,8 +69,8 @@ export default PostExcerpt
 
 PostExcerpt.propTypes = {
   post: PropTypes.shape({
-    featuredImage: PropTypes.object,
-    title: PropTypes.object.isRequired,
+    cover: PropTypes.object,
+    title: PropTypes.string.isRequired,
     slug: PropTypes.string.isRequired,
     author: PropTypes.shape({
       name: PropTypes.string.isRequired,
@@ -77,7 +79,7 @@ PostExcerpt.propTypes = {
       photo: PropTypes.object.isRequired,
     }).isRequired,
     date: PropTypes.string.isRequired,
-    categories: PropTypes.arrayOf(
+    tags: PropTypes.arrayOf(
       PropTypes.shape({
         slug: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
@@ -85,9 +87,4 @@ PostExcerpt.propTypes = {
     ).isRequired,
     body: PropTypes.object.isRequired,
   }),
-  iconSize: PropTypes.string.isRequired,
-}
-
-PostExcerpt.defaultProps = {
-  iconSize: `1em`,
 }
