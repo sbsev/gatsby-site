e169e0f64389299b96c8b3a6c6160a60e36d0ec0
JanoshRiebesell <janosh.riebesell@gmail.com>
2019-01-31 09:47:44 +0100

    refactor: chapters page

--- src/pages/standorte.js
+++ src/pages/standorte.js
@@ -1,65 +1,64 @@
-import React, { Component } from 'react'
-import { graphql, Link } from 'gatsby'
+import React from "react"
+import { graphql, Link } from "gatsby"
 
-import Global from '../components/Global'
-import PageTitle from '../components/PageTitle'
-import Map from '../components/Map'
-import PageBody from '../components/styles/PageBody'
-import PageMeta from '../components/PageMeta'
-import Chapters from '../components/styles/Chapters'
+import Global from "../components/Global"
+import PageTitle from "../components/PageTitle"
+import Map from "../components/Map"
+import PageBody from "../components/styles/PageBody"
+import PageMeta from "../components/PageMeta"
+import Chapters from "../components/styles/Chapters"
 
-export default class ChaptersPage extends Component {
-  addMarkers = map => {
-    let chapterCount = 1
-    this.props.data.chapters.edges.forEach(({ node }) => {
-      const marker = new window.google.maps.Marker({
-        map,
-        position: node.coords,
-        label: `${chapterCount}`,
-        title: node.title,
-      })
-      marker.addListener(`click`, () => {
-        window.location.href = `/standorte/` + node.slug
-      })
-      ++chapterCount
+const addMarkers = chapters => map => {
+  let chapterCount = 1
+  chapters.forEach(({ node }) => {
+    const marker = new window.google.maps.Marker({
+      map,
+      position: node.coords,
+      label: `${chapterCount}`,
+      title: node.title,
     })
-  }
-
-  mapProps = {
-    options: {
-      center: { lat: 51, lng: 10 },
-      zoom:
-        // checking that window is defined necessary for compiling on server
-        typeof window !== `undefined` &&
-        5 + Math.min(window.innerWidth, window.innerHeight) / 1000,
-    },
-    onMount: this.addMarkers,
-  }
-
-  render() {
-    const { data, location } = this.props
-    const { page, chapters } = data
-    const { title, body } = page
-    const { excerpt, html } = body.data
-    return (
-      <Global pageTitle={title} path={location.pathname} description={excerpt}>
-        <PageTitle>
-          <h1>{title}</h1>
-        </PageTitle>
-        <Map id="chapterMap" {...this.mapProps} />
-        <Chapters>
-          {chapters.edges.map(({ node }) => (
-            <li key={node.slug}>
-              <Link to={`/standorte/` + node.slug}>{node.title}</Link>
-            </li>
-          ))}
-        </Chapters>
-        {html && <PageBody dangerouslySetInnerHTML={{ __html: html }} />}
-        <PageMeta {...page} />
-      </Global>
-    )
-  }
+    marker.addListener(`click`, () => {
+      window.location.href = `/standorte/` + node.slug
+    })
+    ++chapterCount
+  })
 }
+
+const mapProps = chapters => ({
+  options: {
+    center: { lat: 51, lng: 10 },
+    zoom:
+      // checking that window is defined necessary for compiling on server
+      typeof window !== `undefined` &&
+      5 + Math.min(window.innerWidth, window.innerHeight) / 1000,
+  },
+  onMount: addMarkers(chapters),
+})
+
+const ChaptersPage = ({ data, location }) => {
+  const { page, chapters } = data
+  const { title, body } = page
+  const { excerpt, html } = body.data
+  return (
+    <Global pageTitle={title} path={location.pathname} description={excerpt}>
+      <PageTitle>
+        <h1>{title}</h1>
+      </PageTitle>
+      <Map id="chapterMap" {...mapProps(chapters.edges)} />
+      <Chapters>
+        {chapters.edges.map(({ node }) => (
+          <li key={node.slug}>
+            <Link to={`/standorte/` + node.slug}>{node.title}</Link>
+          </li>
+        ))}
+      </Chapters>
+      {html && <PageBody dangerouslySetInnerHTML={{ __html: html }} />}
+      <PageMeta {...page} />
+    </Global>
+  )
+}
+
+export default ChaptersPage
 
 export const query = graphql`
   {
