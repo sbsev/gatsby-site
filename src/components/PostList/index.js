142b7bbf784b5ed128c0a90c258b8186da6c9345
Janosh Riebesell <janosh.riebesell@gmail.com>
2019-02-04 14:15:13 +0100

    feat: PostList remove Masonry layout, use grid instead

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,16 +1,13 @@
 import React from "react"
 
-import Masonry from "../Masonry"
 import PostExcerpt from "../PostExcerpt"
 import { Container } from "./styles"
 
 const PostList = ({ posts, ...rest }) => (
   <Container>
-    <Masonry>
-      {posts.map(({ node }) => (
-        <PostExcerpt key={node.slug} post={node} {...rest} />
-      ))}
-    </Masonry>
+    {posts.map(({ node }) => (
+      <PostExcerpt key={node.slug} post={node} {...rest} />
+    ))}
   </Container>
 )
 
--- src/components/PostList/styles.js
+++ src/components/PostList/styles.js
@@ -1,7 +1,11 @@
-import styled from 'styled-components'
-import mediaQuery from '../../utils/mediaQuery'
+import styled from "styled-components"
+
+import mediaQuery from "../../utils/mediaQuery"
 
 export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(17em, 1fr));
+  grid-gap: 1em;
   font-size: 0.8em;
   line-height: 1.4em;
   grid-column: 2/4;
