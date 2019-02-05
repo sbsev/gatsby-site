1499a0442fff8adfdaac23774e83ba38a7880144
Janosh Riebesell <janosh.riebesell@gmail.com>
2019-02-05 22:23:39 +0100

    fix: PostList layout when displaying single post

--- src/components/PostList/styles.js
+++ src/components/PostList/styles.js
@@ -2,14 +2,15 @@ import styled from "styled-components"
 
 import mediaQuery from "../../utils/mediaQuery"
 
-export const Container = styled.div`
+export const PostListContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(17em, 1fr));
-  grid-gap: 1em;
+  grid-gap: 2em 1.5em;
   font-size: 0.8em;
   line-height: 1.4em;
+  height: max-content;
   grid-column: 2/4;
-  ${mediaQuery.maxPhone} {
-    grid-column: 3;
+  ${mediaQuery.maxTablet} {
+    grid-column: 3/4;
   }
 `
