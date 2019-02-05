4517599811e1c8d0ecae0f7ec0074ac04fd11d0b
Janosh Riebesell <janosh.riebesell@gmail.com>
2019-02-05 22:23:39 +0100

    fix: PostList layout when displaying single post

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,14 +1,17 @@
 import React from "react"
 
 import PostExcerpt from "../PostExcerpt"
-import { Container } from "./styles"
+import Grid from "../styles/Grid"
+import { PostListContainer } from "./styles"
 
 const PostList = ({ posts, ...rest }) => (
-  <Container>
-    {posts.map(({ node }) => (
-      <PostExcerpt key={node.slug} post={node} {...rest} />
-    ))}
-  </Container>
+  <PostListContainer>
+    <Grid minWidth="17em" maxWidth="24em" gap="2em 1.5em">
+      {posts.map(({ node }) => (
+        <PostExcerpt key={node.slug} post={node} {...rest} />
+      ))}
+    </Grid>
+  </PostListContainer>
 )
 
 export default PostList
