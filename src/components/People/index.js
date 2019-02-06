03716375076bfb37804dee092a61f97d3313591b
Janosh Riebesell <janosh.riebesell@gmail.com>
2019-02-06 14:41:28 +0100

    feat: add People comp

---
+++ src/components/People/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Person from "../Person"
+import { Wrapper } from "./styles"
+
+const People = ({ people }) => (
+  <Wrapper>
+    {people.map(person => (
+      <Person key={person.name} {...person} />
+    ))}
+  </Wrapper>
+)
+
+export default People
