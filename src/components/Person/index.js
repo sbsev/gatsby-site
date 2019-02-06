9d3f8cf875e57cc5b0bcc6ac074636913aa707cc
Janosh Riebesell <janosh.riebesell@gmail.com>
2019-02-06 14:41:20 +0100

    feat: add Person comp

---
+++ src/components/Person/index.js
@@ -0,0 +1,41 @@
+import React from "react"
+import { CalendarDay } from "styled-icons/fa-solid/CalendarDay"
+import { Link } from "styled-icons/boxicons-regular/Link"
+import { Email } from "styled-icons/material/Email"
+
+import { PersonContainer, Details, Img, Remits, Name } from "./styles"
+
+const Person = ({ name, remits, photo, bio, email, homepage, dateJoined }) => (
+  <PersonContainer>
+    <Img fluid={photo.fluid} />
+    <Name>{name}</Name>
+    <Remits>{remits.map(remit => remit.title).join(`, `)}</Remits>
+    <Details>
+      {dateJoined && (
+        <span>
+          <CalendarDay size="1em" />
+          &nbsp;Seit {dateJoined} dabei
+        </span>
+      )}
+      {(email || homepage) && (
+        <address>
+          {homepage && (
+            <a href={homepage}>
+              <Link size="1em" />
+              &nbsp;Homepage
+            </a>
+          )}
+          {email && (
+            <a href={`mailto:${email}`}>
+              <Email size="1em" />
+              &nbsp;Email
+            </a>
+          )}
+        </address>
+      )}
+      {bio && <span dangerouslySetInnerHTML={{ __html: bio.remark.html }} />}
+    </Details>
+  </PersonContainer>
+)
+
+export default Person
---
+++ src/components/Person/styles.js
@@ -0,0 +1,56 @@
+import styled from "styled-components"
+import Image from "gatsby-image"
+
+export const PersonContainer = styled.div`
+  text-align: center;
+  line-height: 1.2em;
+  position: relative;
+`
+
+export const Details = styled.div`
+  position: absolute;
+  top: 105%;
+  padding: 0.5em;
+  transform: translateX(-50%);
+  left: 50%;
+  opacity: 0;
+  visibility: hidden;
+  background: ${props => props.theme.lightestGray};
+  z-index: 2;
+  max-width: 90vw;
+  min-width: 15em;
+  border-radius: ${props => props.theme.mediumBorderRadius};
+  transition: ${props => props.theme.mediumTrans} ease-in-out;
+  transition-delay: 0.3s;
+  display: grid;
+  grid-gap: 0.4em;
+  ${PersonContainer}:hover & {
+    opacity: 1;
+    visibility: visible;
+  }
+  span p:first-child {
+    margin: 0;
+  }
+  address > :not(:first-child) {
+    margin-left: 1em;
+  }
+`
+
+export const Img = styled(Image)`
+  border-radius: 50%;
+  transition: ${props => props.theme.mediumTrans} ease-in-out;
+  margin-bottom: 0.3em;
+  :hover {
+    transform: scale(1.05);
+  }
+`
+
+export const Name = styled.span`
+  display: block;
+  margin-bottom: 0.2em;
+`
+
+export const Remits = styled.span`
+  display: block;
+  font-weight: lighter;
+`
