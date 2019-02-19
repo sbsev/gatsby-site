d1a640784b1818ee9701534007575938783e2957
janosh <janosh.riebesell@gmail.com>
2019-02-19 17:15:59 +0000

    style: typography reduce line heights

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,8 +3,8 @@ const typography = {
   // font sizes and line heigths in em units
   minFontSize: 0.9,
   maxFontSize: 1.2,
-  minLineHeight: 1.4,
-  maxLineHeight: 1.8,
+  minLineHeight: 1.3,
+  maxLineHeight: 1.7,
 }
 
 export default typography
