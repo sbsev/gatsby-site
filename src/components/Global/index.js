1cfb362cbd7fe08ed5596b89987507707f26fe2d
janosh <janosh.riebesell@gmail.com>
2019-03-06 22:09:39 +0000

    refactor: Scroll comp

--- src/components/Global/index.js
+++ src/components/Global/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import { ThemeProvider } from 'styled-components'
-import PropTypes from 'prop-types'
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import { ThemeProvider } from "styled-components"
+import PropTypes from "prop-types"
 
-import Helmet from '../Helmet'
-import Header from '../Header'
-import Footer from '../Footer'
-import theme from '../../utils/theme'
-import { Content, GlobalStyle } from './styles'
-import Scroll from '../Scroll'
+import Helmet from "../Helmet"
+import Header from "../Header"
+import Footer from "../Footer"
+import theme from "../../utils/theme"
+import { Content, GlobalStyle } from "./styles"
+import Scroll from "../Scroll"
 
 const Global = ({ children, site, ...rest }) => (
   <ThemeProvider theme={theme}>
