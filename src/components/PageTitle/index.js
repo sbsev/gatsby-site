8e29a7285d1a110884fd849114cb0ee24cb6f3e2
janosh <janosh.riebesell@gmail.com>
2019-03-07 21:07:20 +0000

    feat: redesign IndexPage PageTitle

--- src/components/PageTitle/index.js
+++ src/components/PageTitle/index.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useRef, useEffect } from "react"
 
-import { Title } from './styles'
+import Caption from "../styles/Caption"
+import { PageTitleContainer, Title, Img } from "./styles"
 
-const PageTitle = ({ children, cols }) => <Title cols={cols}>{children}</Title>
+const PageTitle = ({ children, img, backdrop, className, fillToBottom }) => {
+  const ref = useRef()
+  if (fillToBottom) {
+    const fillAvailHeight = () =>
+      (ref.current.style.height =
+        window.innerHeight - ref.current.offsetTop + `px`)
+    useEffect(() => {
+      fillAvailHeight()
+      window.addEventListener(`resize`, fillAvailHeight)
+      return () => window.removeEventListener(`resize`, fillAvailHeight)
+    })
+  }
+  return (
+    <PageTitleContainer ref={ref} className={className}>
+      <Img {...img} />
+      <Title backdrop={backdrop || (img && img.backdrop)}>{children}</Title>
+      {img && (img.caption || img.credit) && (
+        <Caption showOnHoverParent={PageTitleContainer}>
+          <span dangerouslySetInnerHTML={{ __html: img.caption }} />
+          {img.caption && img.credit && ` | `}
+          {img.credit && (
+            <span>
+              Credit: <a href={img.url}>{img.credit}</a>
+            </span>
+          )}
+        </Caption>
+      )}
+    </PageTitleContainer>
+  )
+}
 
 export default PageTitle
