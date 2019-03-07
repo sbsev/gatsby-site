bfd312dacdc0f0887093214d8516cfe180c1f074
janosh <janosh.riebesell@gmail.com>
2019-03-07 21:01:00 +0000

    feat: add Dots comp

---
+++ src/components/Dots/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import { DotsContainer, Dot } from "./styles"
+
+const Dots = ({ length, current, onClick, size = `0.8em`, topDots }) => (
+  <DotsContainer topDots={topDots}>
+    {Array.apply(null, { length }).map((dot, index) => (
+      <Dot
+        key={index}
+        active={index === current}
+        onClick={() => onClick(index)}
+        size={size}
+      />
+    ))}
+  </DotsContainer>
+)
+
+export default Dots
+
+Dots.propTypes = {
+  length: PropTypes.number.isRequired,
+  current: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
+  size: PropTypes.string,
+}
