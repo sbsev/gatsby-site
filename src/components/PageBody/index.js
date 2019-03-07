1ff5d41bad757a7372cfa9c5ea8fb6ceb97ecb98
janosh <janosh.riebesell@gmail.com>
2019-03-07 21:02:14 +0000

    refactor: move Global Content style into PageBody

--- src/components/PageBody/index.js
+++ src/components/PageBody/index.js
@@ -3,11 +3,11 @@ import React from "react"
 import { BodyContainer, Updated } from "./styles"
 
 const PageBody = ({ children, html, updated }) => (
-  <>
+  <BodyContainer>
     {children}
-    <BodyContainer dangerouslySetInnerHTML={{ __html: html }} />
+    <div dangerouslySetInnerHTML={{ __html: html }} />
     {updated && <Updated>Zuletzt bearbeitet: {updated}</Updated>}
-  </>
+  </BodyContainer>
 )
 
 export default PageBody
