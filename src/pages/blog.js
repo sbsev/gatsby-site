6906dbd9f9c7adb4729a08c36bc73ddf9f683247
janosh <janosh.riebesell@gmail.com>
2019-03-07 21:05:31 +0000

    feat: BlogPage use PageBody and read active tag from url query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,29 +1,44 @@
-import React, { useState } from "react"
-import { graphql } from "gatsby"
+import React, { useState } from 'react'
+import { graphql } from 'gatsby'
 
-import Global from "../components/Global"
-import PageTitle from "../components/PageTitle"
-import TagList from "../components/TagList"
-import PostList from "../components/PostList"
+import Global from '../components/Global'
+import PageTitle from '../components/PageTitle'
+import PageBody from '../components/PageBody'
+import TagList from '../components/TagList'
+import PostList from '../components/PostList'
 
-const BlogPage = ({ data, location, title = `Blog` }) => {
+const filterPostsByTag = (tag, posts) =>
+  tag === `alle`
+    ? posts
+    : posts.filter(({ node }) => node.tags.map(tag => tag.title).includes(tag))
+
+const readActiveTagFromUrl = urlParams =>
+  urlParams.replace(/.*tag=([^&]+).*/, `$1`)
+
+const BlogPage = ({ data, location }) => {
   const { posts, tags } = data
-  const [tag, setTag] = useState(`Alle`)
-  const filteredPosts =
-    tag === `Alle`
-      ? posts.edges
-      : posts.edges.filter(({ node }) =>
-        node.tags.map(tag => tag.title).includes(tag)
-      )
+  const urlTag = readActiveTagFromUrl(location.search)
+  const [tag, setTag] = useState(urlTag || `alle`)
+  const filteredPosts = filterPostsByTag(tag, posts.edges)
+
+  const handleTagClick = tag => {
+    setTag(tag)
+    history.replaceState(
+      { activeTag: tag },
+      `active tag: ${tag}`,
+      tag === `alle` ? `/blog` : `/blog?tag=${tag}`
+    )
+  }
+
   return (
-    <Global pageTitle={title} path={location.pathname}>
-      <PageTitle cols="2 / -2">
-        <h1>{title}</h1>
+    <Global pageTitle="Blog" path={location.pathname}>
+      <PageTitle>
+        <h1>Blog</h1>
       </PageTitle>
-      <TagList tags={tags.edges} activeTag={tag} setTag={setTag} />
-      {posts && (
-        <PostList posts={filteredPosts} activeTag={tag} setTag={setTag} />
-      )}
+      <PageBody>
+        <TagList tags={tags.edges} activeTag={tag} setTag={handleTagClick} />
+        <PostList inBlog posts={filteredPosts} />
+      </PageBody>
     </Global>
   )
 }
