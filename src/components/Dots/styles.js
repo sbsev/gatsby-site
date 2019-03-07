697b160b214dd0e302cc9ac6d01253c77f9b3a1b
janosh <janosh.riebesell@gmail.com>
2019-03-07 21:01:00 +0000

    feat: add Dots comp

---
+++ src/components/Dots/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components"
+
+export const DotsContainer = styled.div`
+  grid-area: dots;
+  display: grid;
+  grid-gap: 1vw;
+  grid-auto-flow: column;
+  justify-self: center;
+  position: absolute;
+  ${props => (props.topDots ? `top` : `bottom`)}: 1em;
+  margin: auto;
+  left: 50%;
+  transform: translate(-50%);
+`
+
+export const Dot = styled.div`
+  border-radius: 50%;
+  height: ${props => props.size};
+  width: ${props => props.size};
+  background: rgba(0, 0, 0, 0.5);
+  background: ${props => props.active && props.theme.lightGreen};
+  transition: ${props => props.theme.mediumTrans};
+  border: 1px solid ${props => props.theme.white};
+  :hover {
+    color: ${props => props.theme.lightBlue};
+    background: ${props => props.theme.darkBlue};
+    transform: scale(1.2);
+    border: 1px solid ${props => props.theme.lightBlue};
+  }
+`
