64c25aa6a357c6ec7f35c01f3c04c60ddc0e9bf1
janosh <janosh.riebesell@gmail.com>
2019-03-09 17:02:14 +0000

    chore: remove outsourced fragments from post, wikiArticle

--- src/templates/post.js
+++ src/templates/post.js
@@ -19,38 +19,6 @@ const PostTemplate = ({ data, location }) => {
 export default PostTemplate
 
 export const query = graphql`
-  fragment postFields on ContentfulPost {
-    slug
-    title
-    author {
-      name
-      email
-      homepage
-      photo {
-        fixed(width: 50) {
-          ...GatsbyContentfulFixed_withWebp
-        }
-      }
-    }
-    tags {
-      title
-      slug
-    }
-    date(formatString: "D. MMM YYYY", locale: "de")
-    cover {
-      fluid {
-        ...GatsbyContentfulFluid_withWebp
-      }
-      title
-    }
-    body {
-      remark: childMarkdownRemark {
-        html
-        timeToRead
-        excerpt
-      }
-    }
-  }
   query($slug: String!) {
     post: contentfulPost(slug: { eq: $slug }) {
       ...postFields
