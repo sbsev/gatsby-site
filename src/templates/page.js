abe7134d703a3a7f8725cf122c352510fa903211
janosh <janosh.riebesell@gmail.com>
2019-03-09 17:01:28 +0000

    refactor: page template use new PageTitle and fragment

--- src/templates/page.js
+++ src/templates/page.js
@@ -3,17 +3,36 @@ import { graphql } from "gatsby"
 
 import Global from "../components/Global"
 import PageTitle from "../components/PageTitle"
+import Scroll from "../components/Scroll"
 import PageBody from "../components/PageBody"
+import RichBody from "../components/RichBody"
+
+import { Title, Subtitle } from "../components/styles/IndexPage"
 
 const PageTemplate = ({ data, location }) => {
-  const { title, body, updatedAt } = data.page
+  const { page, updatedAt } = data
+  const { title, subtitle, cover, coverProps, body, richBody } = page
   const { excerpt, html } = body && body.remark
   return (
     <Global pageTitle={title} path={location.pathname} description={excerpt}>
-      <PageTitle>
-        <h1>{title}</h1>
+      <PageTitle
+        images={cover && cover.map(({ title, img }) => ({ title, ...img }))}
+        fillToBottom={coverProps && coverProps.fillToBottom}
+      >
+        <Title>
+          <h1>{title}</h1>
+          <Subtitle
+            dangerouslySetInnerHTML={{
+              __html: subtitle && subtitle.remark.html,
+            }}
+          />
+        </Title>
+        {coverProps && coverProps.showDownArrow && (
+          <Scroll direction="down" to={1} />
+        )}
       </PageTitle>
       <PageBody html={html} updated={updatedAt} />
+      {richBody && <RichBody doc={JSON.parse(richBody.richBody)} />}
     </Global>
   )
 }
@@ -23,15 +42,7 @@ export default PageTemplate
 export const query = graphql`
   query($slug: String!) {
     page: contentfulPage(slug: { eq: $slug }) {
-      title
-      slug
-      body {
-        remark: childMarkdownRemark {
-          excerpt
-          html
-        }
-      }
-      updatedAt(formatString: "D. MMM YYYY", locale: "de")
+      ...pageFields
     }
   }
 `
