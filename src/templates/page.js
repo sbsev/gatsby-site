b79759ef85725873fe6a00f309d67522ef2f8533
janosh <janosh.riebesell@gmail.com>
2019-03-12 20:09:21 +0000

    refactor: pageFields fragment use slideFields

--- src/templates/page.js
+++ src/templates/page.js
@@ -6,12 +6,22 @@ import PageTitle from "../components/PageTitle"
 import PageBody from "../components/PageBody"
 
 const PageTemplate = ({ data, location }) => {
-  const { title, body, updatedAt } = data.page
+  const { page, updatedAt } = data
+  const { title, subtitle, cover, body } = page
   const { excerpt, html } = body && body.remark
   return (
     <Global pageTitle={title} path={location.pathname} description={excerpt}>
-      <PageTitle>
+      <PageTitle
+        images={cover && cover.map(({ img, ...rest }) => ({ ...img, ...rest }))}
+      >
         <h1>{title}</h1>
+        {subtitle && (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: subtitle.remark.html,
+            }}
+          />
+        )}
       </PageTitle>
       <PageBody html={html} updated={updatedAt} />
     </Global>
@@ -23,15 +33,7 @@ export default PageTemplate
 export const query = graphql`
   query($slug: String!) {
     page: contentfulPage(slug: { eq: $slug }) {
-      title
-      slug
-      body {
-        remark: childMarkdownRemark {
-          excerpt
-          html
-        }
-      }
-      updatedAt(formatString: "D. MMM YYYY", locale: "de")
+      ...pageFields
     }
   }
 `
