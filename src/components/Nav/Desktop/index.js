392b1a7738b3392a4497161711595631312f2eec
Janosh Riebesell <janosh.riebesell@gmail.com>
2019-04-19 19:03:57 +0200

    feat: rewrite Nav using react-spring on mobile

---
+++ src/components/Nav/Desktop/index.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { DownArrow } from "styled-icons/boxicons-regular/DownArrow"
+
+import { NavEntry, SubNav, NavLink, DesktopNavDiv } from "./styles"
+
+export default ({ nav }) => (
+  <DesktopNavDiv>
+    {nav.map(({ url, title, subNav }) => (
+      <NavEntry key={url}>
+        <NavLink to={url || subNav[0].url} title={title}>
+          {title} {subNav && <DownArrow size="0.5em" />}
+        </NavLink>
+        {subNav && (
+          <SubNav>
+            {subNav.map(item => (
+              <NavLink
+                key={item.url}
+                to={url + item.url}
+                title={item.title}
+                span={item.span}
+              >
+                {item.title}
+              </NavLink>
+            ))}
+          </SubNav>
+        )}
+      </NavEntry>
+    ))}
+  </DesktopNavDiv>
+)
