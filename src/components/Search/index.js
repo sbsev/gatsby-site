23652fd1cfb03bb5005dcf9a4897895a6ae7e023
janosh <janosh.riebesell@gmail.com>
2019-05-26 13:57:37 +0100

    refactor: Search, MobileNav outsource useClickOutside hook

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createRef } from "react"
+import React, { useState, createRef } from "react"
 import {
   InstantSearch,
   Index,
@@ -7,6 +7,7 @@ import {
 } from "react-instantsearch-dom"
 import algoliasearch from "algoliasearch/lite"
 
+import { useClickOutside } from "../../utils/hooks"
 import { Root, HitsWrapper, PoweredBy } from "./styles"
 import Input from "./Input"
 import * as hitComps from "./hitComps"
@@ -28,20 +29,7 @@ export default function Search({ indices, collapse, hitsAsGrid }) {
     process.env.GATSBY_ALGOLIA_APP_ID,
     process.env.GATSBY_ALGOLIA_SEARCH_KEY
   )
-
-  const handleClickOutside = event =>
-    !ref.current.contains(event.target) && setFocus(false)
-
-  useEffect(() => {
-    [`mousedown`, `touchstart`].forEach(event =>
-      document.addEventListener(event, handleClickOutside)
-    )
-    return () =>
-      [`mousedown`, `touchstart`].forEach(event =>
-        document.removeEventListener(event, handleClickOutside)
-      )
-  })
-
+  useClickOutside(ref, () => setFocus(false))
   return (
     <InstantSearch
       searchClient={searchClient}
