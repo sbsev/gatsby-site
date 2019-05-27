e193a62249f9f5fab846d27f829b2dfbccc01f60
janosh <janosh.riebesell@gmail.com>
2019-05-27 21:43:07 +0100

    style: PageBody minPhablet justify text only within paragraphs

--- src/components/PageBody/styles.js
+++ src/components/PageBody/styles.js
@@ -40,7 +40,9 @@ export const Body = styled.main`
     grid-column: 3;
   }
   ${mediaQuery.minPhablet} {
-    text-align: justify;
+    p {
+      text-align: justify;
+    }
   }
   p > img {
     max-width: 100%;
