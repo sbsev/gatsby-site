916c2b7db73235eff8562739a24d9ab2b6d4ca97
janosh <janosh.riebesell@gmail.com>
2019-05-27 21:45:27 +0100

    refactor: convert page components from arrow to normal functions

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,21 +2,23 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Global from "../components/Global"
-import PostTitle from "../components/PostTitle"
+import PageTitle from "../components/PageTitle"
+import PostMeta from "../components/PostMeta"
 import PageBody from "../components/PageBody"
 
-const PostTemplate = ({ data, location }) => {
-  const { title, body } = data.post
-  const { html, excerpt } = body.remark
+export default function PostTemplate({ data, location }) {
+  const { title, author, date, cover, body } = data.post
+  const { html, excerpt, timeToRead } = body.remark
   return (
     <Global pageTitle={title} path={location.pathname} description={excerpt}>
-      <PostTitle {...data} />
+      <PageTitle cover={cover}>
+        <h1>{title}</h1>
+        <PostMeta inTitle {...{ author, date, timeToRead }} />
+      </PageTitle>
       <PageBody html={html} />
     </Global>
   )
 }
-
-export default PostTemplate
 
 export const query = graphql`
   query($slug: String!) {
