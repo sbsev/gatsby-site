99ee2067e4da7fcad231a7841b2802cfef5b7b79
janosh <janosh.riebesell@gmail.com>
2019-05-27 22:00:22 +0100

    refactor: revert react-spring Slideshow to pure CSS implementation

---
+++ src/components/Slideshow/styles.js
@@ -0,0 +1,38 @@
+import styled from "styled-components"
+
+export const Slides = styled.div`
+  position: relative;
+  overflow: hidden;
+  height: 100%;
+  width: 100%;
+  z-index: 10;
+  display: grid;
+`
+
+export const Slide = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  opacity: ${props => (props.active ? 1 : 0)};
+  visibility: ${props => (props.active ? `visible` : `hidden`)};
+  transition: 1s ease-in-out;
+  z-index: -1;
+  animation: ken-burns ${props => 6 * props.duration}s linear infinite;
+  @keyframes ken-burns {
+    0% {
+      transform: scale(1) translate(0);
+    }
+    25% {
+      transform: scale(1.1) translate(3%, 1%);
+    }
+    50% {
+      transform: scale(1) translate(0);
+    }
+    75% {
+      transform: scale(1.1) translate(-3%, -1%);
+    }
+    100% {
+      transform: scale(1) translate(0);
+    }
+  }
+`
