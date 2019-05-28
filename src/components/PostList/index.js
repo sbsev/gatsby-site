4ff0ca64de901bafd931f100cde0db7933e972e1
janosh <janosh.riebesell@gmail.com>
2019-05-28 10:17:37 +0100

    style: PostList & TagList

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -6,7 +6,7 @@ import { PostListContainer } from "./styles"
 
 const PostList = ({ posts, ...rest }) => (
   <PostListContainer>
-    <Grid minWidth="17em" maxWidth="24em" gap="2em 1.5em">
+    <Grid minWidth="18em" maxWidth="24em" gap="1.5em">
       {posts.map(({ node }) => (
         <PostExcerpt key={node.slug} post={node} {...rest} />
       ))}
