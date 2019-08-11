6d938bbedce8771bb5d4eb83127ab5d53787fc58
janosh <janosh.riebesell@gmail.com>
2019-08-11 15:53:01 +0100

    refactor: Footer query for files

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,41 +1,9 @@
-import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import PropTypes from "prop-types"
-
-import Link from "../Link"
-import { FooterContainer, FooterLinks, Source, PoweredBy } from "./styles"
-import Social from "../Social"
-import { navLinkStyle } from "../Nav"
-
-const Footer = ({ copyright, source, links, poweredBy, logos }) => (
-  <FooterContainer>
-    <div>
-      © {new Date().getFullYear()} - {copyright}
-    </div>
-    <FooterLinks>
-      {links.map(link => (
-        <Link css={navLinkStyle} key={link.url} to={link.url}>
-          {link.title}
-        </Link>
-      ))}
-    </FooterLinks>
-    <Social iconCss={navLinkStyle} />
-    <Source dangerouslySetInnerHTML={{ __html: source }} />
-    <PoweredBy>
-      Powered by:{` `}
-      {poweredBy.map(({ url, title }, index) => (
-        <a key={title} href={url}>
-          <img src={logos[index].node.file.url} alt={title} />
-        </a>
-      ))}
-    </PoweredBy>
-  </FooterContainer>
-)
-
-Footer.propTypes = {
-  copyright: PropTypes.string.isRequired,
-  links: PropTypes.array.isRequired,
-}
+import { graphql, useStaticQuery } from 'gatsby'
+import React from 'react'
+import Link from '../Link'
+import { navLinkStyle } from '../Nav'
+import Social from '../Social'
+import { FooterContainer, FooterLinks, PoweredBy, Source } from './styles'
 
 const query = graphql`
   {
@@ -52,28 +20,40 @@ const query = graphql`
           title
         }
       }
-    }
-    logos: allContentfulAsset(
-      filter: { title: { regex: "/Footer logo/" } }
-      sort: { fields: title }
-    ) {
-      logos: edges {
-        node {
-          title
-          file {
-            url
-          }
+      files {
+        file {
+          url
         }
       }
     }
   }
 `
 
-export default props => (
-  <StaticQuery
-    query={query}
-    render={({ footer, logos }) => (
-      <Footer {...footer.data} {...logos} {...props} />
-    )}
-  />
-)
+export default function Footer() {
+  const { footer } = useStaticQuery(query)
+  const { copyright, source, links, poweredBy } = footer.data
+  return (
+    <FooterContainer>
+      <div>
+        © {new Date().getFullYear()} - {copyright}
+      </div>
+      <FooterLinks>
+        {links.map(link => (
+          <Link css={navLinkStyle} key={link.url} to={link.url}>
+            {link.title}
+          </Link>
+        ))}
+      </FooterLinks>
+      <Social iconCss={navLinkStyle} />
+      <Source dangerouslySetInnerHTML={{ __html: source }} />
+      <PoweredBy>
+        Powered by:{` `}
+        {poweredBy.map(({ url, title }, index) => (
+          <a key={title} href={url}>
+            <img src={footer.files[index].file.url} alt={title} />
+          </a>
+        ))}
+      </PoweredBy>
+    </FooterContainer>
+  )
+}
