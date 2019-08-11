5e67eaac84bc6cfbbecb2c6e5c1ac62d1fef526b
janosh <janosh.riebesell@gmail.com>
2019-08-11 15:53:01 +0100

    refactor: Footer query for files

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,79 +1,57 @@
+import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import PropTypes from 'prop-types'
-
 import Link from '../Link'
-import { FooterDiv, FooterLinks, Source, PoweredBy } from './styles'
-import Social from '../Social'
 import { navLinkStyle } from '../Nav'
+import Social from '../Social'
+import { FooterDiv, FooterLinks, PoweredBy, Source } from './styles'
 
-const Footer = ({ copyright, source, links, poweredBy, logos }) => (
-  <FooterDiv>
-    <div>
-      © {new Date().getFullYear()} - {copyright}
-    </div>
-    <FooterLinks>
-      {links.map(link => (
-        <Link css={navLinkStyle} key={link.url} to={link.url}>
-          {link.title}
-        </Link>
-      ))}
-    </FooterLinks>
-    <Social iconCss={navLinkStyle} />
-    <Source dangerouslySetInnerHTML={{ __html: source }} />
-    <PoweredBy>
-      Powered by:{` `}
-      {poweredBy.map(({ url, title }, index) => (
-        <a key={title} href={url}>
-          <img src={logos[index].node.file.url} alt={title} />
-        </a>
-      ))}
-    </PoweredBy>
-  </FooterDiv>
-)
-
-Footer.propTypes = {
-  copyright: PropTypes.string.isRequired,
-  links: PropTypes.array.isRequired,
-}
-
-const query = graphql`
-  {
-    footer: contentfulJson(title: { eq: "Footer" }) {
-      data {
-        copyright
-        source
-        links {
-          url
-          title
+export default function Footer() {
+  const { footer } = useStaticQuery(graphql`
+    {
+      footer: contentfulJson(title: { eq: "Footer" }) {
+        data {
+          copyright
+          source
+          links {
+            url
+            title
+          }
+          poweredBy {
+            url
+            title
+          }
         }
-        poweredBy {
-          url
-          title
-        }
-      }
-    }
-    logos: allContentfulAsset(
-      filter: { title: { regex: "/Footer logo/" } }
-      sort: { fields: title }
-    ) {
-      logos: edges {
-        node {
-          title
+        files {
           file {
             url
           }
         }
       }
     }
-  }
-`
-
-export default props => (
-  <StaticQuery
-    query={query}
-    render={({ footer, logos }) => (
-      <Footer {...footer.data} {...logos} {...props} />
-    )}
-  />
-)
+  `)
+  const { copyright, source, links, poweredBy } = footer.data
+  return (
+    <FooterDiv>
+      <div>
+        © {new Date().getFullYear()} - {copyright}
+      </div>
+      <FooterLinks>
+        {links.map(link => (
+          <Link css={navLinkStyle} key={link.url} to={link.url}>
+            {link.title}
+          </Link>
+        ))}
+      </FooterLinks>
+      <Social iconCss={navLinkStyle} />
+      <Source dangerouslySetInnerHTML={{ __html: source }} />
+      <PoweredBy>
+        Powered by:{` `}
+        {poweredBy.map(({ url, title }, index) => (
+          <a key={title} href={url}>
+            <img src={footer.files[index].file.url} alt={title} />
+          </a>
+        ))}
+      </PoweredBy>
+    </FooterDiv>
+  )
+}
