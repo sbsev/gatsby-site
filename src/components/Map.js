5d6798afe7794baaf17e3512378641e804db94a5
janosh <janosh.riebesell@gmail.com>
2019-08-13 18:20:28 +0100

    feat: Map comp use cookie-free google maps api

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef } from 'react'
 
 export default function Map({ options, onMount, className }) {
   const props = { ref: useRef(), className }
@@ -11,7 +11,7 @@ export default function Map({ options, onMount, className }) {
     if (!window.google) {
       const script = document.createElement(`script`)
       script.type = `text/javascript`
-      script.src = `https://maps.google.com/maps/api/js?key=${
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${
         process.env.GATSBY_GOOGLE_MAPS_API_KEY
       }`
       const headScript = document.getElementsByTagName(`script`)[0]
