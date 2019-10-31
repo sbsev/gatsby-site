c849b203fe8a6d3c69257108a1f334e44df845fc
janosh <janosh.riebesell@gmail.com>
2019-10-31 16:53:28 +0000

    refactor: titleCase, algolia transformer

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,9 @@
 export const paramCase = str => str.toLowerCase().replace(` `, `-`)
 
-export const titleCase = str =>
-  str
-    ? str
-      .split(` `)
-      .map(word => word[0].toUpperCase() + word.substr(1).toLowerCase())
-      .join(` `)
-    : str
+export const titleCase = str => {
+  if (!str) return str
+  return str
+    .split(` `)
+    .map(word => word[0].toUpperCase() + word.substr(1).toLowerCase())
+    .join(` `)
+}
