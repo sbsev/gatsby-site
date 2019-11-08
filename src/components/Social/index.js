865d5db495f0d3169954cece2bb78594178ac61a
janosh <janosh.riebesell@gmail.com>
2019-11-08 18:38:40 +0000

    refactor: convert remaining StaticQuery to useStaticQuery

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -1,48 +1,36 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import { Wrapper, Container, Toggle, Icons } from './styles'
 
-const Social = ({ social, size = `1em`, iconCss, collapse, short }) => (
-  <Wrapper>
-    {collapse && <Toggle size={size} css={iconCss} />}
-    <Container collapse={collapse}>
-      {Object.keys(social).map(service => {
-        if (short && [`Email`, `Github`].includes(service)) return undefined
-        const Icon = Icons[service]
-        return (
-          <a key={service} href={social[service]} css={iconCss}>
-            <Icon size={size} />
-          </a>
-        )
-      })}
-    </Container>
-  </Wrapper>
-)
-
-const query = graphql`
-  {
-    social: contentfulJson(title: { eq: "Social" }) {
-      social: data {
-        Email
-        Facebook
-        Github
-        Linkedin
-        Youtube
+export default function Social({ size = `1em`, iconCss, collapse, short }) {
+  const { social } = useStaticQuery(graphql`
+    {
+      social: contentfulJson(title: { eq: "Social" }) {
+        data {
+          Email
+          Facebook
+          Github
+          Linkedin
+          Youtube
+        }
       }
     }
-  }
-`
-
-export default props => (
-  <StaticQuery
-    query={query}
-    render={data => <Social {...data.social} {...props} />}
-  />
-)
-
-Social.propTypes = {
-  social: PropTypes.object.isRequired,
-  size: PropTypes.string,
+  `)
+  return (
+    <Wrapper>
+      {collapse && <Toggle size={size} css={iconCss} />}
+      <Container collapse={collapse}>
+        {Object.keys(social.data).map(service => {
+          if (short && [`Email`, `Github`].includes(service)) return undefined
+          const Icon = Icons[service]
+          return (
+            <a key={service} href={social[service]} css={iconCss}>
+              <Icon size={size} />
+            </a>
+          )
+        })}
+      </Container>
+    </Wrapper>
+  )
 }
