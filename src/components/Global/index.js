7d97b57cebf0c7a315098ebc7ed19977ce378d38
janosh <janosh.riebesell@gmail.com>
2019-11-08 18:38:40 +0000

    refactor: convert remaining StaticQuery to useStaticQuery

--- src/components/Global/index.js
+++ src/components/Global/index.js
@@ -1,5 +1,4 @@
-import { graphql, StaticQuery } from 'gatsby'
-import PropTypes from 'prop-types'
+import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import theme from 'utils/theme'
@@ -10,36 +9,29 @@ import Scroll from '../Scroll'
 import Seo from '../Seo'
 import { GlobalStyle } from './styles'
 
-const Global = ({ children, site, ...rest }) => (
-  <ThemeProvider theme={theme}>
-    <>
-      <Seo site={site.meta} {...rest} />
-      <GlobalStyle />
-      <BrowserCheck />
-      <Header site={site.meta} />
-      {children}
-      <Footer />
-      <Scroll showBelow={1500} css="position: fixed; right: 1em;" />
-    </>
-  </ThemeProvider>
-)
-
-Global.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-const query = graphql`
-  {
-    site {
-      meta: siteMetadata {
-        title
-        url
-        description
+export default function Global({ children, ...rest }) {
+  const { site } = useStaticQuery(graphql`
+    {
+      site {
+        meta: siteMetadata {
+          title
+          url
+          description
+        }
       }
     }
-  }
-`
-
-export default props => (
-  <StaticQuery query={query} render={data => <Global {...data} {...props} />} />
-)
+  `)
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <Seo site={site.meta} {...rest} />
+        <GlobalStyle />
+        <BrowserCheck />
+        <Header site={site.meta} />
+        {children}
+        <Footer />
+        <Scroll showBelow={1500} css="position: fixed; right: 1em;" />
+      </>
+    </ThemeProvider>
+  )
+}
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import mediaQuery from 'utils/mediaQuery'
 
@@ -9,7 +8,43 @@ import DesktopNav from './Desktop'
 
 export { navLinkStyle, NavLink } from './styles'
 
-const Nav = props => {
+export default function Nav(props) {
+  let { nav, chapters } = useStaticQuery(graphql`
+    {
+      nav: contentfulJson(title: { eq: "Nav" }) {
+        data {
+          nav {
+            url
+            title
+            subNav {
+              url
+              title
+              span
+            }
+          }
+        }
+      }
+      chapters: allContentfulChapter(
+        filter: { active: { eq: true } }
+        sort: { fields: title, order: ASC }
+      ) {
+        edges {
+          node {
+            title
+            slug
+          }
+        }
+      }
+    }
+  `)
+  // clone nav and merge chapters
+  // merging chapters without cloning: results in chapters compounding on every page navigation
+  chapters = chapters.edges.map(({ node }) => ({
+    title: node.title,
+    url: `/` + node.slug,
+  }))
+  nav = JSON.parse(JSON.stringify(nav.data.nav))
+  nav.find(el => el.url === `/standorte`).subNav.unshift(...chapters)
   if (typeof window !== `undefined`) {
     const query = window.matchMedia(mediaQuery.maxNetbookJs)
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -20,68 +55,10 @@ const Nav = props => {
       query.addListener(handleMatch)
       return () => query.removeListener(handleMatch)
     })
-    return mobile ? <MobileNav {...props} /> : <DesktopNav {...props} />
+    return mobile ? (
+      <MobileNav {...props} nav={nav} />
+    ) : (
+      <DesktopNav {...props} nav={nav} />
+    )
   } else return null
-}
-
-const query = graphql`
-  {
-    nav: contentfulJson(title: { eq: "Nav" }) {
-      data {
-        nav {
-          url
-          title
-          subNav {
-            url
-            title
-            span
-          }
-        }
-      }
-    }
-    chapters: allContentfulChapter(
-      filter: { active: { eq: true } }
-      sort: { fields: title, order: ASC }
-    ) {
-      edges {
-        node {
-          title
-          slug
-        }
-      }
-    }
-  }
-`
-
-export default props => (
-  <StaticQuery
-    query={query}
-    render={({ nav, chapters }) => {
-      // clone nav and merge chapters
-      // merging chapters without cloning: results in chapters compounding on every page navigation
-      chapters = chapters.edges.map(({ node }) => ({
-        title: node.title,
-        url: `/` + node.slug,
-      }))
-      nav = JSON.parse(JSON.stringify(nav.data.nav))
-      nav.find(el => el.url === `/standorte`).subNav.unshift(...chapters)
-      return <Nav nav={nav} {...props} role="navigation" />
-    }}
-  />
-)
-
-Nav.propTypes = {
-  nav: PropTypes.arrayOf(
-    PropTypes.shape({
-      url: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      subNav: PropTypes.arrayOf(
-        PropTypes.shape({
-          url: PropTypes.string.isRequired,
-          title: PropTypes.string.isRequired,
-          span: PropTypes.string,
-        })
-      ),
-    })
-  ).isRequired,
 }
