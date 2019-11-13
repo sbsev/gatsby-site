00b35c72ccff733e2627e7c1613ebdcc213d6269
janosh <janosh.riebesell@gmail.com>
2019-11-13 18:05:54 +0000

    feat: add Toc component to page template

---
+++ src/components/Toc/styles.js
@@ -0,0 +1,99 @@
+import styled, { css } from 'styled-components'
+import { BookContent } from 'styled-icons/boxicons-regular/BookContent'
+import { Close as Cross } from 'styled-icons/material/Close'
+import mediaQuery from 'utils/mediaQuery'
+
+const openTocDiv = css`
+  background: white;
+  color: ${props => props.theme.textColor};
+  padding: 0.7em 1.2em;
+  border-radius: 0.5em;
+  box-shadow: 0 0 1em rgba(0, 0, 0, 0.5);
+  border: 1px solid ${props => props.theme.borderColor};
+`
+
+export const TocDiv = styled.div`
+  height: max-content;
+  z-index: 1;
+  line-height: 2em;
+  right: 1em;
+  max-width: 20em;
+  max-height: 80vh;
+  nav {
+    max-height: 78vh;
+    overflow-y: scroll;
+  }
+  ${mediaQuery.maxLaptop} {
+    position: fixed;
+    bottom: 1em;
+    left: 1em;
+    ${props => !props.open && `height: 0;`};
+    ${props => props.open && openTocDiv};
+    visibility: ${props => (props.open ? `visible` : `hidden`)};
+    opacity: ${props => (props.open ? 1 : 0)};
+    transition: ${props => props.theme.shortTrans};
+  }
+  ${mediaQuery.minLaptop} {
+    font-size: 0.85em;
+    grid-column: 4 / -1;
+    position: sticky;
+    top: 7em;
+  }
+`
+
+export const Title = styled.h2`
+  margin: 0;
+  padding-bottom: 0.5em;
+  display: grid;
+  grid-auto-flow: column;
+  align-items: center;
+  grid-template-columns: auto auto 1fr;
+`
+
+export const TocLink = styled.a`
+  color: ${({ theme, active }) => (active ? theme.linkColor : theme.textColor)};
+  font-weight: ${props => props.active && `bold`};
+  display: block;
+  margin-left: ${props => props.depth + `em`};
+  border-top: ${props =>
+    props.depth === 0 && `1px solid ` + props.theme.lighterGray};
+`
+
+export const TocIcon = styled(BookContent)`
+  width: 1em;
+  margin-right: 0.2em;
+`
+
+const openedCss = css`
+  position: fixed;
+  bottom: 1em;
+  left: 0;
+  padding: 0.5em 0.6em 0.5em 0.3em;
+  background: ${props => props.theme.background};
+  z-index: 1;
+  box-shadow: 0 0 1em ${props => props.theme.shadowColor};
+  border: 1px solid ${props => props.theme.borderColor};
+  border-radius: 0 50% 50% 0;
+  transform: translate(${props => (props.open ? `-100%` : 0)});
+`
+
+const closedCss = css`
+  margin-left: 1em;
+  border: 1px solid ${props => props.theme.borderColor};
+  border-radius: 50%;
+`
+
+export const Toggle = styled(Cross).attrs(props => ({
+  as: props.opener && BookContent,
+  size: props.size || `1.2em`,
+}))`
+  transition: ${props => props.theme.shortTrans};
+  justify-self: end;
+  :hover {
+    transform: scale(1.1);
+  }
+  ${mediaQuery.minLaptop} {
+    display: none;
+  }
+  ${props => (props.opener ? openedCss : closedCss)};
+`
--- src/utils/fragments/page.js
+++ src/utils/fragments/page.js
@@ -21,6 +21,7 @@ export const query = graphql`
         html
       }
     }
+    toc
     body {
       remark: childMarkdownRemark {
         excerpt
