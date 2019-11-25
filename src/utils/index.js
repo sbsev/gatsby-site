60914b1e2c1b8af7c0540ec1ee74746558656c74
janosh <janosh.riebesell@gmail.com>
2019-11-25 20:35:32 +0000

    style: larger buttons, scale on hover

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-export const paramCase = str => str.toLowerCase().replace(` `, `-`)
-
-export const titleCase = str => {
-  if (!str) return str
-  return str
-    .split(` `)
-    .map(word => word[0].toUpperCase() + word.substr(1).toLowerCase())
-    .join(` `)
-}
