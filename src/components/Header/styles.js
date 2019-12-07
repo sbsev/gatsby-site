b1cb901f928ab8e5615228f59c28b76e5a8853a1
Janosh Riebesell <janosh.riebesell@gmail.com>
2019-12-07 16:23:54 +0000

    fix: prevent DesktopNav to appear on mobile after page reload from clean cache, refactor MobileNav to position sticky outside header in lower left corner of screen

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,41 +1,28 @@
 import styled from 'styled-components'
-import { Link } from 'gatsby'
 
 import mediaQuery from 'utils/mediaQuery'
 import Owl from 'assets/logo'
-import { navLinkStyle } from '../Nav'
 
 export const HeaderContainer = styled.header`
+  z-index: 3;
+  font-size: 1.2em;
   background: ${props => props.theme.darkBlue};
   display: grid;
   grid-gap: calc(1em + 1vw);
   align-items: center;
   justify-items: center;
-  grid-template-areas: 'nav title social search';
-  grid-template-columns: auto 1fr auto auto;
+  grid-template-areas: 'title social search';
+  grid-template-columns: 1fr auto auto;
   padding: calc(0.25em + 1vh) calc(1em + 1vw);
   font-weight: bold;
   ${mediaQuery.minNetbook} {
     grid-template-areas: 'title nav social search';
-  }
-`
-
-export const SiteTitle = styled(Link)`
-  grid-area: title;
-  display: grid;
-  align-items: center;
-  grid-auto-flow: column;
-  grid-gap: 1em;
-  ${navLinkStyle};
-  ${mediaQuery.minTablet} {
-    grid-auto-flow: column;
+    grid-template-columns: auto 1fr auto auto;
   }
 `
 
 export const Logo = styled(Owl)`
   height: 3em;
-  width: 3em;
-  object-position: top;
   border-radius: 50%;
   background: white;
   padding: 0.1em;
--- src/components/Nav/Mobile/styles.js
+++ src/components/Nav/Mobile/styles.js
@@ -1,71 +1,56 @@
 import { animated } from 'react-spring'
 import styled, { css } from 'styled-components'
-import { Close } from 'styled-icons/material/Close'
-import { KeyboardArrowDown as More } from 'styled-icons/material/KeyboardArrowDown'
-import { KeyboardArrowRight as Arrow } from 'styled-icons/material/KeyboardArrowRight'
-import { KeyboardArrowUp as Less } from 'styled-icons/material/KeyboardArrowUp'
 import { ThMenu } from 'styled-icons/typicons/ThMenu'
-import mediaQuery from 'utils/mediaQuery'
 import { NavLink } from '../styles'
+import { Close as Cross } from 'styled-icons/material/Close'
+import mediaQuery from 'utils/mediaQuery'
 
 export { NavLink }
-
-const iconCss = css`
-  width: 1em;
-  margin-right: 0.2em;
-  cursor: pointer;
-  vertical-align: -0.15em;
-`
-
-export const Icons = {
-  More: styled(More)`
-    ${iconCss};
-    color: ${props => props.theme.lighterGreen};
-  `,
-  Less: styled(Less)`
-    ${iconCss};
-    color: ${props => props.theme.lightBlue};
-  `,
-  Arrow: styled(Arrow)`
-    ${iconCss};
-  `,
-}
+export { KeyboardArrowDown as ArrowDown } from 'styled-icons/material/KeyboardArrowDown'
+export { KeyboardArrowUp as ArrowUp } from 'styled-icons/material/KeyboardArrowUp'
 
 export const MobileNavDiv = styled.nav`
+  z-index: 1;
+  overscroll-behavior: none;
+  box-sizing: border-box;
+  width: 15em;
+  max-width: 80vw;
   position: fixed;
-  z-index: 1; /* places nav on top of search and social icon in header */
   top: 0;
   overflow: scroll;
-  -webkit-overflow-scrolling: touch;
-  height: 120vh;
-  background: rgba(0, 0, 0, 0.85);
-  padding: 8vmin 8vmin 8vmin 6vmin;
-  box-sizing: border-box;
-  font-size: 1.6em;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.9);
+  padding: 2em;
+  font-size: 1.2em;
   color: white;
   right: 100%;
   display: grid;
   grid-gap: 1em;
-  min-width: 20vw;
-  grid-auto-columns: max-content;
   grid-auto-rows: max-content;
-  transform: translate(${props => (props.open ? `100%` : `0`)});
+  transform: translate(${props => (props.open ? `99%` : `0`)});
   transition: ${props => props.theme.shortTrans};
-  ${mediaQuery.maxNetbook} {
-    max-width: 13em;
+  line-height: 1.4em;
+  /* Needed to scroll past last element in case of overflow. */
+  :after {
+    content: '';
+    height: 0.5em;
   }
-  ${mediaQuery.maxPhone} {
-    justify-content: center;
-    text-align: center;
-    width: 100%;
-    max-width: initial;
+`
+
+export const Item = styled.div`
+  /* Target arrow icons prefixing nav links with children. */
+  svg:first-child {
+    width: 1em;
+    margin-right: 0.3em;
+    cursor: pointer;
+    vertical-align: -0.1em;
   }
 `
 
 export const Children = styled(animated.div)`
   will-change: transform, opacity, height;
   margin-left: 0.5em;
-  padding-left: 0.5em;
+  padding-left: 0.8em;
   border-left: thin dashed white;
   overflow: hidden;
   padding-bottom: ${props => props.open && `0.6em`};
@@ -76,21 +61,36 @@ export const Children = styled(animated.div)`
   }
 `
 
-export const Menu = styled(NavLink).attrs({
-  size: `1em`,
-  as: ThMenu,
-})`
-  cursor: pointer;
-  transform: scale(1.7, 1.3);
-  margin: 0 0.3em;
+const openedCss = css`
+  position: fixed;
+  bottom: 2vh;
+  left: 0;
+  padding: 0.6em 0.9em 0.6em 0.4em;
+  background: ${props => props.theme.darkBlue};
+  border-radius: 0 50% 50% 0;
+  transform: translate(${props => (props.open ? `-100%` : 0)});
+  border: 1px solid;
+  border-left: none;
 `
 
-export const Closer = styled(Close)`
+const closedCss = css`
+  color: white;
   position: absolute;
-  top: 0.8em;
-  right: 1em;
-  width: 1.5em;
-  ${mediaQuery.minPhone} {
+  top: 1em;
+  right: 1.3em;
+`
+
+export const NavToggle = styled(Cross).attrs(props => ({
+  as: props.opener && ThMenu,
+  size: `1.5em`,
+}))`
+  color: white;
+  transition: ${props => props.theme.shortTrans};
+  :hover {
+    transform: scale(1.1);
+  }
+  ${mediaQuery.minLaptop} {
     display: none;
   }
+  ${props => (props.opener ? openedCss : closedCss)};
 `
