f09429562fd6b73c92ace086e03de20921e699ae
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-01-12 11:17:09 +0000

    feat: optimize Map to rerender only when necessary

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,24 +1,51 @@
-import React, { useEffect, useRef } from 'react'
+import { functions, isEqual, omit } from 'lodash'
+import React, { useState, useEffect, useRef } from 'react'
 
-export default function Map({ options, onMount, className }) {
-  const props = { ref: useRef(), className }
-  const onLoad = () => {
-    const map = new window.google.maps.Map(props.ref.current, options)
-    onMount && onMount(map)
-  }
+function Map({ options, onMount, className, onMountProps }) {
+  const ref = useRef()
+  const [map, setMap] = useState()
 
   useEffect(() => {
+    // The Google Maps API modifies the options object passed to
+    // the Map constructor in place by adding a mapTypeId with default
+    // value 'roadmap'. { ...options } prevents this by creating a copy.
+    const onLoad = () =>
+      setMap(new window.google.maps.Map(ref.current, { ...options }))
     if (!window.google) {
       const script = document.createElement(`script`)
-      script.type = `text/javascript`
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GATSBY_GOOGLE_MAPS_API_KEY}`
-      const headScript = document.getElementsByTagName(`script`)[0]
-      headScript.parentNode.insertBefore(script, headScript)
+      script.src =
+        `https://maps.googleapis.com/maps/api/js?key=` +
+        process.env.GATSBY_GOOGLE_MAPS_API_KEY
+      document.head.append(script)
       script.addEventListener(`load`, onLoad)
       return () => script.removeEventListener(`load`, onLoad)
     } else onLoad()
-  })
+  }, [options])
 
-  const mapCss = `height: 70vh; margin: 1em 0; border-radius: 0.5em;`
-  return <div css={mapCss} {...props} />
+  if (map && typeof onMount === `function`) onMount(onMountProps)(map)
+
+  return (
+    <div
+      css="height: 60vh; margin: 1em 0; border-radius: 0.5em;"
+      {...{ ref, className }}
+    />
+  )
+}
+
+function shouldNotUpdate(props, nextProps) {
+  const [funcs, nextFuncs] = [functions(props), functions(nextProps)]
+  const noPropChange = isEqual(omit(props, funcs), omit(nextProps, nextFuncs))
+  const noFuncChange =
+    funcs.length === nextFuncs.length &&
+    funcs.every(fn => props[fn].toString() === nextProps[fn].toString())
+  return noPropChange && noFuncChange
+}
+
+export default React.memo(Map, shouldNotUpdate)
+
+Map.defaultProps = {
+  options: {
+    center: { lat: 48, lng: 8 },
+    zoom: 5,
+  },
 }
