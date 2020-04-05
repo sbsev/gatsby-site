c51aaf1ec9ec41d36aa381e6c07615e3fa7fd663
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-04-05 12:34:58 +0200

    refactor: PageBody remove unnecessary wrapper div

--- src/components/PageBody/index.js
+++ src/components/PageBody/index.js
@@ -1,23 +1,20 @@
 import React from 'react'
 
-import { Body, Updated, Address } from './styles'
+import { Main, Content, Updated, Address } from './styles'
 
 export default function PageBody(props) {
-  const { children, mainChildren, html, updatedAt, title } = props
+  const { children, html, updatedAt, title } = props
   const mailto = `mailto:it@studenten-bilden-schueler.de?subject=Feedback zu Seite "${title}"`
   return (
-    <Body>
+    <Main>
       {children}
-      <main>
-        {mainChildren}
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-        {updatedAt && <Updated>Zuletzt bearbeitet: {updatedAt}</Updated>}
-        {title && (
-          <Address>
-            <a href={mailto}>War diese Seite hilfreich?</a>
-          </Address>
-        )}
-      </main>
-    </Body>
+      <Content dangerouslySetInnerHTML={{ __html: html }} />
+      {updatedAt && <Updated>Zuletzt bearbeitet: {updatedAt}</Updated>}
+      {title && (
+        <Address>
+          <a href={mailto}>War diese Seite hilfreich?</a>
+        </Address>
+      )}
+    </Main>
   )
 }
