1c27fc17962dc2b22f00beee0ee42927e4683ed8
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-06-04 11:49:09 +0200

    chore: eslint add no-var, spaced-comment rules, remove globalThis (now supported by default)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,37 @@
-/*
-#!/bin/sh
-#
-# Pre-commit hook called by "git commit" with no arguments.
-# Checks staged .js(x) files for eslint errors.
-# Exits with non-zero status after issuing an appropriate message if
-# it wants to stop the commit.
-# Place in {projectRoot}/.git/hooks/pre-commit
-
-git diff --diff-filter=d --cached --name-only | grep "\.js.\?$" | xargs ./node_modules/.bin/eslint
-*/
+// These settings are used by a git pre-commit hook in src/utils/gitHooks
+// which ensures eslint compliance before allowing commits.
 
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es2020: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parser: 'babel-eslint',
+  extends: [`eslint:recommended`, `plugin:react/recommended`],
+  parser: `babel-eslint`,
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: `module`,
   },
-  plugins: ['react', 'react-hooks'],
+  plugins: [`react`, `react-hooks`],
   rules: {
-    indent: ['error', 2, { SwitchCase: 1 }],
-    quotes: ['error', 'backtick', { avoidEscape: true }],
-    semi: ['error', 'never'],
-    'linebreak-style': ['error', 'unix'],
-    'react/prop-types': 'off',
-    'react/display-name': 'off',
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    indent: [`error`, 2, { SwitchCase: 1 }],
+    quotes: [`error`, `backtick`, { avoidEscape: true }],
+    semi: [`error`, `never`],
+    'linebreak-style': [`error`, `unix`],
+    'react/prop-types': `off`,
+    'react/display-name': `off`,
+    'no-console': [`error`, { allow: [`warn`, `error`] }],
+    'react-hooks/rules-of-hooks': `error`,
+    'react-hooks/exhaustive-deps': `warn`,
+    'no-var': `error`,
+    'spaced-comment': [`error`, `always`],
   },
   settings: {
     react: {
-      version: 'detect',
+      version: `detect`,
     },
-  },
-  globals: {
-    globalThis: false, // false means it is not writeable.
-    // See https://github.com/eslint/eslint/issues/11553.
   },
 }
